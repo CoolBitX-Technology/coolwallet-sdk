ef1284ff8354a3053838716f0f7585ed39d29022
Anton Cheng <anton@coolbitx.com>
2019-10-27 22:17:25 +0800

    split apdu device, backup

--- packages/sdk-core/src/apdu/index.js
+++ packages/sdk-core/src/apdu/index.js
@@ -2,5 +2,6 @@ import * as device from './device'
 import * as tx from './transaction'
 import * as wallet from './wallet'
 import * as dfu from './dfu'
+import * as backup from './backup'
 
-export { device, tx, wallet, dfu }
+export { device, tx, wallet, dfu, backup }
