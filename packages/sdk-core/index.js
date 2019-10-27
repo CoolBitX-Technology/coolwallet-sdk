b17c6c7956ffd6bfe24c8a76a5ed6c51dedf269c
Anton Cheng <anton@coolbitx.com>
2019-10-27 22:13:13 +0800

    export wallet, core, apdu

--- packages/sdk-core/index.js
+++ packages/sdk-core/index.js
@@ -1,10 +1,14 @@
 import CWSDevice from './src/Device'
 import CWSWallet from './src/Wallet'
 import { generateKeyPair } from './src/crypto/keyPair'
+import * as core from './src/core'
+import * as apdu from './src/apdu'
 export * from './src/StandardCoin'
 
 export {
   generateKeyPair,
   CWSDevice,
-  CWSWallet
+  CWSWallet,
+  core,
+  apdu
 }
