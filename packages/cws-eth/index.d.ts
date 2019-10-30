feac14bb561588ab977c614e2fd57fe7f48aa5d9
Anton Cheng <anton@coolbitx.com>
2019-10-30 21:21:32 +0800

    update type script

--- packages/cws-eth/index.d.ts
+++ packages/cws-eth/index.d.ts
@@ -3,22 +3,22 @@ declare class cwsETH {
   /**
    * Get Ethereum address by index
    */
-  async getAddress(addressIndex:string) : Promise<string>
+  async getAddress(addressIndex: string): Promise<string>
 
   /**
    * Sign Ethereum Transaction.
    */
-  async signTransaction(payload:string, addressIndex:number, publicKey?:string) : Promise<string>
+  async signTransaction(payload: string, addressIndex: number, publicKey?: string): Promise<string>
 
   /**
    * Sign Arbitrary Message.
    */
-  async signMessage(message:string, addressIndex:number, publicKey?:string, isHashRequired?:Boolean) : Promise<string>
+  async signMessage(message: string, addressIndex: number, publicKey?: string, isHashRequired?: Boolean): Promise<string>
 
   /**
    * Sign EIP712 typed data
    */
-  async signTypedData(typedData: Object,  addressIndex:number, publicKey?:string) : Promise<string>
+  async signTypedData(typedData: Object, addressIndex: number, publicKey?: string): Promise<string>
 }
 
-export default cwsETH
+export = cwsETH
