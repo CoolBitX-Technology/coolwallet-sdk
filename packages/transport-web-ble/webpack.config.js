4404fee0a5c1a43a667f852ba12d6dc22926995e
kunming.liu <kunming@coolbitx.com>
2019-11-09 20:42:23 +0800

    add webpack.config

---
+++ packages/transport-web-ble/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  resolve: {
+    extensions: ['.js'],
+  },
+  output: {
+    path: path.join(__dirname, '/lib'),
+    filename: 'transport-web-ble.min.js',
+    libraryTarget: 'umd',
+    library: 'transport-web-ble',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  }
+};
