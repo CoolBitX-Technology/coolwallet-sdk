7fda93cedecacd1cc47655c34c93c26362fce3e4
kunming.liu <kunming@coolbitx.com>
2019-11-09 19:19:55 +0800

    added webpack.config

---
+++ packages/transport/index.d.ts
@@ -0,0 +1,24 @@
+export default class Transport {
+  constructor(
+    device: any,
+    sendCommandToCard: Function,
+    sendDataToCard: Function,
+    checkCardStatus: Function,
+    readCharacteristic: Function) {
+  }
+  static isSupported(): Promise<boolean>;
+
+  static listen(callback: (error: Error, device: any) => void);
+
+  static connect(deviceOrId: object | string): Promise<Transport>;
+
+  static disconnect(id: string): Promise<void>;
+
+  sendCommandToCard(command: number[]): Promise<void>;
+
+  sendDataToCard(packets: number[]): Promise<void>;
+
+  checkCardStatus(): Promise<number>;
+
+  readDataFromCard(): Promise<number[]>;
+}
