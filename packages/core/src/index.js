fb884933ce78987eba8eba7f7606504dfbe07437
Anton Cheng <anton@coolbitx.com>
2019-11-09 19:16:25 +0700

    test core webpack

---
+++ packages/core/src/index.js
@@ -0,0 +1,11 @@
+import * as core from './core'
+import * as apdu from './apdu'
+import * as config from './config'
+import * as crypto from './crypto'
+
+export {
+  config,
+  crypto,
+  core,
+  apdu
+}
