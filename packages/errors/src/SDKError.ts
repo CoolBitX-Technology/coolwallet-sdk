5f73dc5bd2966b697967f0c6c15a5821420870e5
Anton Cheng <anton@coolbitx.com>
2019-12-27 14:38:32 +0800

    Update SDK Error

--- packages/errors/src/SDKError.ts
+++ packages/errors/src/SDKError.ts
@@ -1,12 +1,10 @@
 export class SDKError extends Error {
   public name:string
-  public code:string
 
-  constructor(message: string, code:string) {
+  constructor(name:string, message: string) {
     super(message);
     Object.setPrototypeOf(this, new.target.prototype);;
     this.constructor = SDKError;
-    this.name = "SDKError";
-    this.code = code
+    this.name = name;
   }
 }
--- packages/errors/src/helper.js
+++ packages/errors/src/helper.js
@@ -1,18 +1,19 @@
 import { SDKError } from './SDKError'
 
-let errorClasses = {}
-
-export const createErrorClass = (name, message, code) => {
+/**
+ * Create CoolWalletSError class
+ * @param {string} name 
+ * @param {string} message 
+ * @return { object }
+ */
+export const createErrorClass = (name, message) => {
   const C = function CoolWalletSError(fields) {
     Object.assign(this, fields);
     this.name = name;
     this.message = message || name;
-    this.stack = new SDKError(message, code).stack;
+    this.stack = new SDKError(name, message).stack;
   };
-  // $FlowFixMe
-  C.prototype = new SDKError(message, code);
+  C.prototype = new SDKError(name, message);
 
-  errorClasses[name] = C;
-  // $FlowFixMe we can't easily type a subset of Error for now...
   return C;
 };
