fb937905340216aaf9f87dd0f685e5f89c565530
Anton Cheng <anton@coolbitx.com>
2019-12-29 20:37:41 +0800

    Remove error config

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -1,6 +1,5 @@
 import COMMAND from './command'
-import ERROR from './error'
 import * as KEY from './key'
 import * as RESPONSE from './response'
 
-export { COMMAND, ERROR, KEY, RESPONSE }
+export { COMMAND, KEY, RESPONSE }
