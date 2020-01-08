8c0d9f292c36031baf20a13c2138c02406d1e7c0
Anton Cheng <anton@coolbitx.com>
2020-01-08 11:14:02 +0800

    Update eslint rule no-restricted-syntax and no-use-before-define

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,73 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jest": true
-    },
-    "globals": {
-        "__DEV__": false,
-        "__TEST__": false,
-        "__PROD__": false,
-        "__COVERAGE__": false,
-        "__dirname": false,
-        "Buffer": true,
-        "process": true
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
   },
-	extends: ["airbnb"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "ecmaVersion": 8,
-        "sourceType": "module"
+  globals: {
+    __DEV__: false,
+    __TEST__: false,
+    __PROD__: false,
+    __COVERAGE__: false,
+    __dirname: false,
+    Buffer: true,
+    process: true
+  },
+  extends: ['airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    "plugins": [ "react" ],
-    "rules": {
-        "no-console":  "off",
-        "no-unused-vars": "warn",
-        "no-unreachable": "warn",
-        "no-case-declarations": "warn",
-        "key-spacing": "warn",
-        "jsx-quotes": [ 2, "prefer-single" ],
-		"object-curly-spacing": [ "warn", "always" ],
-        "comma-dangle": "off",
-        "no-mixed-spaces-and-tabs": "off",
-        "eqeqeq": "warn",
-        "block-scoped-var": "error",
-        "import/extensions": [ // import ts files
-            "error",
-            "ignorePackages",
-            {
-              "jsx": "never",
-              "ts": "never",
-              "tsx": "never"
-            }
-         ]
-    },
-    "settings": {
-        "import/resolver": {
-          "node": {
-            "extensions": [".js", ".jsx", ".ts", ".tsx"]
-          }
-        }
+    ecmaVersion: 8,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'no-use-before-define': ['error', { functions: false, classes: true }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
       },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+      }
+    ],
+    'no-unused-vars': 'warn',
+    'no-unreachable': 'warn',
+    'no-case-declarations': 'warn',
+    'key-spacing': 'warn',
+    'jsx-quotes': [2, 'prefer-single'],
+    'object-curly-spacing': ['warn', 'always'],
+    'comma-dangle': 'off',
+    eqeqeq: 'warn',
+    'block-scoped-var': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
 };
