687b964a1734ebd661bb512cc7a8b6b39b00ad4a
Anton Cheng <anton@coolbitx.com>
2020-02-12 14:36:23 +0800

    Add param types

--- packages/cws-btc/src/types.ts
+++ packages/cws-btc/src/types.ts
@@ -1,4 +1,4 @@
-export type input = {
+export type Input = {
   txId:string,
   vout:number,
   value:number,
@@ -7,7 +7,22 @@ export type input = {
   addressIndex:number
 };
 
-export type output = {
+export type Output = {
   address: string,
   value: number
+}
+
+export type Param = {
+  P2WPKH: {
+    READTYPE: string,
+    HASHTYPE: number,
+    REDEEMTYPE: string,
+  },
+  P2PKH: {
+    READTYPE: string,
+    HASHTYPE: number,
+    REDEEMTYPE: string,
+  },
+  P2: string,
+  DISPLAY_P1: string,
 }
