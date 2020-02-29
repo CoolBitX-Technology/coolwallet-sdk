62d2ea9a472541a6ac7a88d474ede796307c1a9c
Anton Cheng <anton@coolbitx.com>
2020-02-29 14:26:03 +0800

    Use network in contructor

--- packages/cws-btc/src/index.ts
+++ packages/cws-btc/src/index.ts
@@ -1,14 +1,21 @@
 import { ECDSACoin } from '@coolwallets/coin';
 import { pubkeyToP2PKHAddress, pubkeyToSegwitAddress } from './util';
 
+// import { Input, Output, Param } from './types';
+// import * as common from './common';
+// import { BTC as BTCONFIG } from './constants';
+
 type Transport = import('@coolwallets/transport').default;
 
 export default class BTC extends ECDSACoin {
   public network: any;
 
+  // public params: Param;
+
   constructor(transport: Transport, appPrivateKey: string, appId: string, network: any) {
     super(transport, appPrivateKey, appId, '00');
     this.network = network;
+    // this.params = BTCONFIG.PARAMS;
   }
 
   /**
@@ -16,11 +23,11 @@ export default class BTC extends ECDSACoin {
    */
   async getP2PKHAddress(addressIndex: number): Promise<string> {
     const publicKey = await this.getPublicKey(addressIndex);
-    return pubkeyToP2PKHAddress(publicKey);
+    return pubkeyToP2PKHAddress(publicKey, this.network);
   }
 
   async getSegwitAddress(addressIndex: number): Promise<string> {
     const publicKey = await this.getPublicKey(addressIndex);
-    return pubkeyToSegwitAddress(publicKey);
+    return pubkeyToSegwitAddress(publicKey, this.network);
   }
 }
