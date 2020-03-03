34ab2559dbb152ce5266900eb7eca5d1dc825b3d
Anton Cheng <anton@coolbitx.com>
2020-03-03 18:19:06 +0800

    Rename isSupportedERC20Transaction, add util function for amount parsing

--- packages/cws-eth/src/token.js
+++ packages/cws-eth/src/token.js
@@ -1,14 +1,28 @@
 // import { apdu } from '@coolwallets/core';
-// import { handleHex, removeHex0x } from './eth_utils.js';
+import Web3 from 'web3';
+import { handleHex, removeHex0x } from './string_util';
+
+const web3 = new Web3();
 
 /**
  * @param {string} data
  * @return {Boolean}
  */
-export const isSupportedERC20Transaction = (data) => {
+export const isSupportedERC20Method = (data) => {
   const functionHash = data.slice(0, 8);
   if (functionHash === 'a9059cbb' || functionHash === '095ea7b3') return true;
   return false;
+};
+
+/**
+ * Parse erc transfer amount and to address from data
+ * @param {string} data
+ */
+export const parseToAndAmount = (data) => {
+  const params = data.slice(8);
+  const to = params.slice(0, 64).slice(24); // last 20 bytes
+  const amount = params.slice(64).slice(40); // last 12 bytes
+  return { to, amount };
 };
 
 /**
@@ -19,12 +33,11 @@ export const isSupportedERC20Transaction = (data) => {
  * @return {String}
  */
 export const getSetTokenPayload = (contractAddress, symbol, decimals) => {
-  // const unit = handleHex(decimals.toString(16));
-  // const len = handleHex(symbol.length.toString(16));
-  // const symb = handleHex(web3.utils.asciiToHex(symbol));
-  // const setTokenPayload =
-  //  unit + len + web3.utils.padRight(symb, 14, '0') + removeHex0x(contractAddress);
-  // return setTokenPayload;
+  const unit = handleHex(decimals.toString(16));
+  const len = handleHex(symbol.length.toString(16));
+  const symb = handleHex(web3.utils.asciiToHex(symbol));
+  const setTokenPayload = unit + len + web3.utils.padRight(symb, 14, '0') + removeHex0x(contractAddress);
+  return setTokenPayload;
 };
 
 /**
