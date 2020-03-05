7b6fe7b59ffab287482912b4f0fa8dbb7d45ca0b
Anton Cheng <anton@coolbitx.com>
2020-03-05 16:41:54 +0800

    Add scriptable erc20 test

--- packages/cws-eth/tests/sign.script.test.js
+++ packages/cws-eth/tests/sign.script.test.js
@@ -30,8 +30,27 @@ test('should sign transaction with script command ', async () => {
   expect(tx).toBe('0xf86e82031b85012a05f20082520c940644de2a0cf3f11ef6ad89c264585406ea346a96880de0b6b3a76400008025a07cce23b352f3c1f11ef4833e76b3b0cb14ca17bb0097d197b307690a551d19eea0156703269448e84d2a82e07531375896fd6fc6e0478cdda876315611d4cad697');
 });
 
+test('should sign erc20 transfer', async () => {
+  const ercTransaction = {
+    chainId: 1,
+    nonce: '0x324',
+    gasPrice: '0x2540be400',
+    gasLimit: '0x905e',
+    to: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    value: '0x0',
+    data: '0xa9059cbb000000000000000000000000c94f3bebddfc0fd7eac7badb149fad2171b94b6900000000000000000000000000000000000000000000000000000000000003e8',
+    tokenInfo: {
+      symbol: 'DAI',
+      decimals: 18
+    }
+  };
+  const tx = await eth.signTransaction(ercTransaction, 0);
+  expect(tx).toBe('0xf8ab8203248502540be40082905e946b175474e89094c44da98b954eedeac495271d0f80b844a9059cbb000000000000000000000000c94f3bebddfc0fd7eac7badb149fad2171b94b6900000000000000000000000000000000000000000000000000000000000003e826a0841b0f25bad78c3544300eb43396c5420b0e7f2eefac97e7c3578790742aaaf3a0625f11b18f9a2e0a8ed786c2b714e6e2fe22bad2a57636905e5a966df98dff6a');
+});
+
 test('should call new generation functions ', async () => {
-  expect(coreModule.core.flow.prepareSEData).toHaveBeenCalledTimes(0);
-  expect(coreModule.core.flow.sendDataToCoolWallet).toHaveBeenCalledTimes(0);
-  expect(coreModule.core.flow.sendScriptAndDataToCard).toHaveBeenCalledTimes(1);
+  expect(coreModule.core.flow.prepareSEData).not.toHaveBeenCalled();
+  expect(coreModule.core.flow.sendDataToCoolWallet).not.toHaveBeenCalled();
+  // script command
+  expect(coreModule.core.flow.sendScriptAndDataToCard).toHaveBeenCalled();
 });
