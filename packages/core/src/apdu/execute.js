69e085f03322d18a2b3c40bb12242627c7339a17
Anton Cheng <anton@coolbitx.com>
2020-03-11 18:11:59 +0800

    Refactor executecommand to allow recursive call and error check

--- packages/core/src/apdu/execute.js
+++ packages/core/src/apdu/execute.js
@@ -3,6 +3,8 @@ import COMMAND from '../config/command';
 import { assemblyCommandAndData, throwSDKError, SDKUnknownWithCode } from './utils';
 import { RESPONSE, DFU_RESPONSE } from '../config/response';
 import { SHA256 } from '../crypto/hash';
+
+
 /**
  * @param {string} commandName
  * @param {Transport} transport
@@ -14,26 +16,13 @@ const executeAPDU = async (commandName, transport, apdu, commandType) => {
   const response = await transport.request(apdu.command, apdu.data);
   if (commandType === 'SE') {
     const status = response.slice(-4);
-    switch (status) {
-      case RESPONSE.SUCCESS: {
-        const outputData = response.slice(0, -4);
-        return { status, outputData };
-      }
-      case RESPONSE.CANCELED: {
-        throw new OperationCanceled();
-      }
-      default: {
-        // In case of error
-        throwSDKError(commandName, status);
-      }
-    }
-  } else {
-    const status = response.slice(4, 6);
-    const outputData = response.slice(6);
-    if (status !== DFU_RESPONSE.SUCCESS) throw SDKUnknownWithCode(commandName, status);
+    const outputData = response.slice(0, -4);
     return { status, outputData };
   }
-  return false;
+  const status = response.slice(4, 6);
+  const outputData = response.slice(6);
+  if (status !== DFU_RESPONSE.SUCCESS) throw SDKUnknownWithCode(commandName, status);
+  return { status, outputData };
 };
 
 /**
@@ -44,30 +33,50 @@ const executeAPDU = async (commandName, transport, apdu, commandType) => {
  * @param {string} data
  * @param {string} params1
  * @param {string} params2
+ * @param {bool} supportSC
+ * @param {bool} forceUseSC
+ * @returns {Promise<{status: string, outputData: string}>}
  */
-// eslint-disable-next-line import/prefer-default-export
 export const executeCommand = async (
-  transport, commandName, commandType = 'SE', data, params1, params2, supportSC = false
+  transport, commandName, commandType = 'SE', data, params1, params2,
+  supportSC = false,
+  forceUseSC = false,
 ) => {
   const commandParams = COMMAND[commandName];
 
   const P1 = params1 || commandParams.P1;
   const P2 = params2 || commandParams.P2;
 
+  let response;
+
   // data too long: divide and send with SECURE CHANNEL
-  if (supportSC && data.length > 500) {
+  if (forceUseSC || (supportSC && data.length > 500)) {
     const apduHeader = commandParams.CLA + commandParams.INS + P1 + P2;
-    const result = await sendWithSecureChannel(transport, apduHeader, data);
-    return result;
+    response = await sendWithSecureChannel(transport, apduHeader, data, forceUseSC);
+  } else {
+    const apdu = assemblyCommandAndData(commandParams.CLA, commandParams.INS, P1, P2, data);
+
+    // eslint-disable-next-line no-console
+    console.debug(`Execute Command: ${commandName}`);
+    response = await executeAPDU(commandName, transport, apdu, commandType);
   }
 
-  const apdu = assemblyCommandAndData(commandParams.CLA, commandParams.INS, P1, P2, data);
+  switch (response.status) {
+    case RESPONSE.SUCCESS: {
+      return response;
+    }
+    case RESPONSE.CANCELED: {
+      throw new OperationCanceled();
+    }
+    default: {
+      // In case of error
+      throwSDKError(commandName, response.status);
 
-  // eslint-disable-next-line no-console
-  console.debug(`Execute Command: ${commandName}`);
-  const result = await executeAPDU(commandName, transport, apdu, commandType);
-
-  return result;
+      // Only command that would reach here: SC_SEND_SEGMENT
+      // Secure channel encounter command error: return status and outputdata
+      return response;
+    }
+  }
 };
 
 
@@ -77,7 +86,7 @@ export const executeCommand = async (
  * @param {string} apduHeader apdu CLS+INS+P1+P2
  * @param {string} apduData apdu data field
  */
-export const sendWithSecureChannel = async (transport, apduHeader, apduData) => {
+export const sendWithSecureChannel = async (transport, apduHeader, apduData, forceUseSC) => {
   const salt = '88888888';
   const dataToHash = apduHeader.concat(salt, apduData);
   const hash = SHA256(dataToHash).toString('hex');
@@ -86,7 +95,7 @@ export const sendWithSecureChannel = async (transport, apduHeader, apduData) => 
   // const buf = Buffer.allocUnsafe()
   const channelVersion = '01';
   const useSecure = '00';
-  const useSign = '00';
+  const useSign = forceUseSC ? '01' : '00';
 
   const cypherData = channelVersion.concat(useSecure, useSign, packedData);
 
@@ -95,22 +104,22 @@ export const sendWithSecureChannel = async (transport, apduHeader, apduData) => 
   const totalPackages = chunks.length;
 
   // Send Data
-  let cipherReturn;
+  let result;
   for (let i = 0; i < totalPackages; i++) {
     // eslint-disable-next-line no-await-in-loop
-    cipherReturn = await sendFragment(transport, chunks[i], i, totalPackages);
+    result = await sendFragment(transport, chunks[i], i, totalPackages);
   }
+  // Uncatched error in SC_SEND_SEGMENT command. Return to parent executeCommand
+  if (result.status !== RESPONSE.SUCCESS) return result;
 
-  // Confirm integrity of returned Data
-  const confirmHash = cipherReturn.slice(4, 68);
-  const confirmSalt = cipherReturn.slice(68, 76);
-  const apduReturn = cipherReturn.slice(76);
+  const confirmHash = result.outputData.slice(4, 68);
+  const confirmSalt = result.outputData.slice(68, 76);
+  const apduReturn = result.outputData.slice(76);
   if (confirmSalt !== salt) throw new Error('SC: Returned salt check failed');
   const returnedDataHash = SHA256(confirmSalt + apduReturn).toString('hex');
   if (returnedDataHash !== confirmHash) throw new Error('SC: Returned hash check failed');
 
-
-  return apduReturn;
+  return { outputData: apduReturn, status: RESPONSE.SUCCESS };
 };
 
 /**
@@ -123,6 +132,5 @@ export const sendWithSecureChannel = async (transport, apduHeader, apduData) => 
 const sendFragment = async (transport, data, index, totalPackages) => {
   const P1 = index.toString(16).padStart(2, '0');
   const P2 = totalPackages.toString(16).padStart(2, '0');
-  const { outputData } = await executeCommand(transport, 'SC_SEND_SEGMENT', 'SE', data, P1, P2);
-  return outputData;
+  return executeCommand(transport, 'SC_SEND_SEGMENT', 'SE', data, P1, P2, false);
 };
