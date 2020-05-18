c2700731a35dac3a06b1a2f808b2c910ed6dff75
hankliu@coolbitx.com <hankliu@coolbitx.com>
2020-05-18 19:21:45 +0800

    Update for srciptable signing

--- packages/core/index.d.ts
+++ packages/core/index.d.ts
@@ -1,8 +1,11 @@
-declare module '@coolwallets/core' {
-  type Transport = import('@coolwallets/transport').default;
+declare module "@coolwallets/core" {
+  type Transport = import("@coolwallets/transport").default;
   export namespace apdu {
     export namespace coin {
-      export function authGetExtendedKey(transport: Transport, signature: string): Promise<string>;
+      export function authGetExtendedKey(
+        transport: Transport,
+        signature: string
+      ): Promise<string>;
       export function getAccountExtendedKey(
         transport: Transport,
         coinType: string,
@@ -17,7 +20,10 @@ declare module '@coolwallets/core' {
     }
 
     export namespace control {
-      export function sayHi(transport: Transport, appId: string): Promise<Boolean>;
+      export function sayHi(
+        transport: Transport,
+        appId: string
+      ): Promise<Boolean>;
       export function getNonce(transport: Transport): Promise<string>;
       export function cancelAPDU(transport: Transport): Promise<void>;
       export function powerOff(transport: Transport): Promise<void>;
@@ -29,7 +35,10 @@ declare module '@coolwallets/core' {
         data: string,
         P1: string
       ): Promise<string>;
-      export function getPairingPassword(transport: Transport, data: string): Promise<string>;
+      export function getPairingPassword(
+        transport: Transport,
+        data: string
+      ): Promise<string>;
       export function getPairedApps(
         trasnport: Transport,
         signature: string
@@ -38,7 +47,10 @@ declare module '@coolwallets/core' {
         trasnport: Transport,
         appIdWithSig: string
       ): Promise<Boolean>;
-      export function renameDevice(trasnport: Transport, nameWithSig: string): Promise<Boolean>;
+      export function renameDevice(
+        trasnport: Transport,
+        nameWithSig: string
+      ): Promise<Boolean>;
     }
 
     export namespace setting {
@@ -47,9 +59,7 @@ declare module '@coolwallets/core' {
       export function getSEVersion(transport: Transport): Promise<number>;
     }
 
-    export namespace transaction {
-
-    }
+    export namespace transaction {}
   }
 
   export namespace config {
@@ -67,10 +77,13 @@ declare module '@coolwallets/core' {
         commandName: string,
         data: string,
         params1?: string,
-        params2?: string,
-      ): Promise<{signature: string, forceUseSC: boolean}>;
+        params2?: string
+      ): Promise<{ signature: string; forceUseSC: boolean }>;
 
-      export function versionCheck(transport: Transport, requiredSEVersion: number): Promise<void>;
+      export function versionCheck(
+        transport: Transport,
+        requiredSEVersion: number
+      ): Promise<void>;
     }
 
     export namespace flow {
@@ -91,29 +104,45 @@ declare module '@coolwallets/core' {
         authorizedCallback?: Function,
         returnCanonical?: Boolean
       ): Promise<{ r: string; s: string } | string | Buffer>;
+      export function sendScriptAndDataToCard(
+        transport: Transport,
+        appId: string,
+        appPrivateKey: string,
+        script: string,
+        argument: string,
+        isEDDSA?: boolean,
+        txPrepareComplteCallback?: Function,
+        authorizedCallback?: Function,
+        return_canonical?: boolean
+      ): Promise<string>;
     }
 
     export namespace util {
       export function getEncryptedSignatures(
         transport: Transport,
-        TxpPrepCommands: Array<{encodedData:String, P1:String, P2:String}>
-      ):Promise<Array<{encryptedSignature:String, publicKey:String}>>
+        TxpPrepCommands: Array<{ encodedData: String; P1: String; P2: String }>
+      ): Promise<Array<{ encryptedSignature: String; publicKey: String }>>;
 
       export function addressIndexToKeyId(
         coinType: string,
         addressIndex: number
-      ): string
+      ): string;
     }
 
     export namespace controller {
-      export function checkSupportScripts(transport: Transport): Promise<boolean>
+      export function checkSupportScripts(
+        transport: Transport
+      ): Promise<boolean>;
     }
   }
 
   export namespace crypto {
     export namespace encryption {
       export function ECIESenc(recipientPubKey: string, msg: string): string;
-      export function ECIESDec(recipientPrivKey: string, encryptionName: string): Buffer;
+      export function ECIESDec(
+        recipientPrivKey: string,
+        encryptionName: string
+      ): Buffer;
     }
   }
 }
