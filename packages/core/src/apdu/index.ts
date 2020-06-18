8f05a89f4e443dd0370ed8f8f260b75808e1d79b
ina.tsai1 <ian.tsai@coolbitx.com>
2020-06-18 16:22:26 +0800

    暫存“

--- packages/core/src/apdu/index.ts
+++ packages/core/src/apdu/index.ts
@@ -7,7 +7,8 @@ import * as wallet from './wallet';
 import * as dfu from './dfu';
 import * as backup from './backup';
 import * as coin from './coin';
+import * as execute from './execute';
 
 export {
-  display, pairing, control, setting, tx, wallet, dfu, backup, coin
+  display, pairing, control, setting, tx, wallet, dfu, backup, coin, execute
 };
