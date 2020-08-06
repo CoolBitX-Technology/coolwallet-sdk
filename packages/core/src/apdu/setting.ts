f038c19554bdc2229d8e5dc6ee9756c5059e3827
ina.tsai1 <ian.tsai@coolbitx.com>
2020-08-06 16:44:36 +0800

    update api

--- packages/core/src/apdu/setting.ts
+++ packages/core/src/apdu/setting.ts
@@ -12,8 +12,8 @@ import { SDKError, APDUError } from '../error/errorHandle';
  * @param {string} signedCommand
  * @return {Promise<{ status: boolean, statusCode: string, msg: string }>}
  */
-export const backupSeed = async (transport: Transport, signedCommand: string): Promise<boolean> => {
-  const { statusCode, msg } = await executeCommand(transport, commands.BACKUP_REGISTER_DATA, target.SE, signedCommand);
+export const backupSeed = async (transport: Transport, sign: { signature: string, forceUseSC: boolean}): Promise<boolean> => {
+  const { statusCode, msg } = await executeCommand(transport, commands.BACKUP_REGISTER_DATA, target.SE, sign.signature, undefined, undefined, sign.forceUseSC);
   if (statusCode === CODE._9000) {
     return true;
   } else {
@@ -38,12 +38,12 @@ export const recoverSeed = async (transport: Transport): Promise<boolean> => {
 /**
  * check if there's backed up data in SE.
  * @param {Transport} transport
- * @return {Promise<{ status: boolean, statusCode: string, msg: string }>} true: may need recovery after update.
+ * @return {boolean} 01 true: may need recovery after update. 00 false
  */
 export const checkBackupStatus = async (transport: Transport): Promise<boolean> => {
   const { statusCode, msg, outputData } = await executeCommand(transport, commands.CHECK_BACKUP_RECOVER, target.SE);
-  if (outputData === '01') {
-    return true
+  if (statusCode === CODE._9000) {
+    return outputData === '01'? true : false;
   } else {
     throw new APDUError(commands.CHECK_BACKUP_RECOVER, statusCode, msg)
   }
@@ -55,8 +55,8 @@ export const checkBackupStatus = async (transport: Transport): Promise<boolean> 
  * @param {string} signedCommand
  * @return {Promise<{ status: boolean, statusCode: string, msg: string }>}
  */
-export const deleteSeedBackup = async (transport: Transport, signedCommand: string) => {
-  const { statusCode, msg } = await executeCommand(transport, commands.DELETE_REGISTER_BACKUP, target.SE, signedCommand);
+export const deleteSeedBackup = async (transport: Transport, sign: { signature: string, forceUseSC: boolean }) => {
+  const { statusCode, msg } = await executeCommand(transport, commands.DELETE_REGISTER_BACKUP, target.SE, sign.signature, undefined, undefined, sign.forceUseSC);
   if (statusCode === CODE._9000) {
     return true
   } else {
