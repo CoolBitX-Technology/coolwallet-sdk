e766cb208e4bd2bc978bf73953fcf8e616c14650
ina.tsai1 <ian.tsai@coolbitx.com>
2020-09-15 10:26:32 +0800

    update

--- packages/cws-ltc/src/index.ts
+++ packages/cws-ltc/src/index.ts
@@ -8,7 +8,7 @@ import {
 	pubkeyToAddressAndOutScript
 } from './util';
 
-// import { signTransaction } from './sign';
+import { signTransaction } from './sign';
 
 type Transport = transport.default;
 
@@ -28,48 +28,50 @@ export default class LTC extends COIN.ECDSACoin implements COIN.Coin {
 	async getAddress(transport: Transport, appPrivateKey: string, appId: string, scriptType: ScriptType, addressIndex: number)
 		: Promise<string> {
 		const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
-		const { address } = pubkeyToAddressAndOutScript(publicKey, scriptType);
+		const { address } = pubkeyToAddressAndOutScript(Buffer.from(publicKey, 'hex'), scriptType);
 		return address;
 	}
 
 	async getAddressAndOutScript(transport: Transport, appPrivateKey: string, appId: string, scriptType: ScriptType, addressIndex: number)
 		: Promise<{ address: string }> {
 		const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
-		return pubkeyToAddressAndOutScript(publicKey, scriptType);
+		return pubkeyToAddressAndOutScript(Buffer.from(publicKey, 'hex'), scriptType);
 	}
+
 
 	async signTransaction(
-	// 	transport: Transport,
-	// 	appPrivateKey: string,
-	// 	appId: string,
-	// 	scriptType: ScriptType,
-	// 	inputs: [Input],
-	// 	output: Output,
-	// 	change?: Change,
-	// 	confirmCB?: Function,
-	// 	authorizedCB?: Function,
+		transport: Transport,
+		appPrivateKey: string,
+		appId: string,
+		scriptType: ScriptType,
+		inputs: [Input],
+		output: Output,
+		change?: Change,
+		confirmCB?: Function,
+		authorizedCB?: Function,
 
-	)  {
-	// 	for (const input of inputs) {
-	// 		// eslint-disable-next-line no-await-in-loop
-	// 		const pubkey = await this.getPublicKey(transport, appPrivateKey, appId, input.addressIndex);
-	// 		input.pubkeyBuf = Buffer.from(pubkey, 'hex');
-	// 	}
-	// 	if (change) {
-	// 		const pubkey = await this.getPublicKey(transport, appPrivateKey, appId, change.addressIndex);
-	// 		// eslint-disable-next-line no-param-reassign
-	// 		change.pubkeyBuf = Buffer.from(pubkey, 'hex');
-	// 	}
-	// 	return signTransaction(
-	// 		transport,
-	// 		appId,
-	// 		appPrivateKey,
-	// 		scriptType,
-	// 		inputs,
-	// 		output,
-	// 		change,
-	// 		confirmCB,
-	// 		authorizedCB
-	// 	);
+	): Promise<string> {
+		for (const input of inputs) {
+			console.log("input: " + input.preValue)
+			const pubkey = await this.getPublicKey(transport, appPrivateKey, appId, input.addressIndex);
+			input.pubkeyBuf = Buffer.from(pubkey, 'hex');
+		}
+		if (change) {
+			console.log("change: " + change.value)
+			const pubkey = await this.getPublicKey(transport, appPrivateKey, appId, change.addressIndex);
+			change.pubkeyBuf = Buffer.from(pubkey, 'hex');
+		}
+		return signTransaction(
+			transport,
+			appId,
+			appPrivateKey,
+			scriptType,
+			inputs,
+			output,
+			change,
+			confirmCB,
+			authorizedCB
+		);
 	}
 }
+
