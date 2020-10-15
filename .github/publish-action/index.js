3df222ce05383ef8f1c02c6c751dc11a47ee9bee
Quincy Chang <quincy0710@hotmail.com>
2020-10-15 18:32:42 +0800

    chore: add qkc auto deployment

--- .github/publish-action/index.js
+++ .github/publish-action/index.js
@@ -76,8 +76,8 @@ async function run() {
 	await checkAndPublish(context, 'packages/cws-zen');
 	await checkAndPublish(context, 'packages/cws-eos');
 	await checkAndPublish(context, 'packages/cws-eth');
-	await checkAndPublish(context, 'packages/cws-qkc');
 	await checkAndPublish(context, 'packages/cws-icx');
+	await checkAndPublish(context, 'packages/cws-qkc');
 	await checkAndPublish(context, 'packages/cws-xlm');
 	await checkAndPublish(context, 'packages/cws-xrp');
 	await checkAndPublish(context, 'packages/transport-react-native-ble');
