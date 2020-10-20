abb5bee9012c1bf7a3f9215b05b81e75aabca389
ina.tsai1 <ian.tsai@coolbitx.com>
2020-10-20 15:57:40 +0800

    update type

--- packages/cws-eth/src/type.ts
+++ packages/cws-eth/src/type.ts
@@ -38,33 +38,6 @@ export type signTyped = {
   authorizedCB: Function | undefined
 }
 
-export type typedData = {
-  type: 'object',
-  properties: {
-    types: {
-      type: 'object',
-      properties: {
-        EIP712Domain: { type: 'array' },
-      },
-      additionalProperties: {
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            name: { type: 'string' },
-            type: { type: 'string' }
-          },
-          required: ['name', 'type']
-        }
-      },
-      required: ['EIP712Domain']
-    },
-    primaryType: { type: 'string' },
-    domain: { type: 'object' },
-    message: { type: 'object' }
-  },
-  required: ['types', 'primaryType', 'domain', 'message']
-};
 
 export type Transaction = {
   // [key: string]: any,
