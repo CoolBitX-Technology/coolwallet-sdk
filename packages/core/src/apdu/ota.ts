a3d565652bab900980caf400a18cb992d6170399
Quincy Chang <quincy0710@hotmail.com>
2021-05-27 15:21:58 +0800

    feat: update se firmware to 309

--- packages/core/src/apdu/ota.ts
+++ packages/core/src/apdu/ota.ts
@@ -15,7 +15,7 @@ import { SDKError } from '../error/errorHandle';
 
 var jwt = require('jsonwebtoken');
 
-const SE_UPDATE_VER = 308;
+const SE_UPDATE_VER = 309;
 
 const challengeUrl = `https://ota.cbx.io/api/challenge`;
 const cryptogramUrl = `https://ota.cbx.io/api/cryptogram`;
