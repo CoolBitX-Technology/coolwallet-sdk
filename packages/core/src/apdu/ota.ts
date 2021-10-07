22eb3a5225cc29e7350e511fe9889e74728c66ee
Quincy Chang <quincy0710@hotmail.com>
2021-10-07 15:01:20 +0800

    fix(core): update se firmware 315 to fix baseConvert hex zeroInherit

--- packages/core/src/apdu/ota.ts
+++ packages/core/src/apdu/ota.ts
@@ -15,7 +15,7 @@ import { SDKError } from '../error/errorHandle';
 
 var jwt = require('jsonwebtoken');
 
-const SE_UPDATE_VER = 314;
+const SE_UPDATE_VER = 315;
 
 const challengeUrl = `https://ota.cbx.io/api/challenge`;
 const cryptogramUrl = `https://ota.cbx.io/api/cryptogram`;
