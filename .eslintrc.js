e09d9a65852b90e3cd379ea55c65f99e907779f7
hank121314 <hank121314@gmail.com>
2021-11-23 17:47:30 +0800

    chore(*): add .prettierrc and modify .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,69 +1,68 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     commonjs: true,
     es6: true,
     jest: true,
   },
-  extends: ["airbnb", "plugin:@typescript-eslint/recommended"],
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
       jsx: true,
     },
     ecmaVersion: 8,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "no-use-before-define": ["error", { functions: false, classes: true }],
-    "no-restricted-syntax": [
-      "error",
+    'max-len': ['error', { code: 120 }],
+    'no-use-before-define': ['error', { functions: false, classes: true }],
+    'no-restricted-syntax': [
+      'error',
       {
-        selector: "ForInStatement",
+        selector: 'ForInStatement',
         message:
-          "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
       },
       {
-        selector: "LabeledStatement",
-        message:
-          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
       },
       {
-        selector: "WithStatement",
-        message:
-          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
       },
     ],
-    "no-use-before-define": "off",
-    "no-unused-vars": "off",
-    "no-unreachable": "warn",
-    "no-case-declarations": "warn",
-		"no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error", { "ignoreTypeValueShadow": true }],
-    "key-spacing": "warn",
-    "jsx-quotes": [2, "prefer-single"],
-    "object-curly-spacing": ["warn", "always"],
-    "comma-dangle": "off",
-    eqeqeq: "warn",
-    "block-scoped-var": "error",
-    "import/prefer-default-export": "off",
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }], //off
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'no-use-before-define': 'off',
+    'no-unused-vars': 'off',
+    'no-unreachable': 'warn',
+    'no-case-declarations': 'warn',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error', { ignoreTypeValueShadow: true }],
+    'key-spacing': 'warn',
+    'jsx-quotes': [2, 'prefer-single'],
+    'object-curly-spacing': ['warn', 'always'],
+    'comma-dangle': 'off',
+    eqeqeq: 'warn',
+    'block-scoped-var': 'error',
+    'import/prefer-default-export': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }], //off
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
