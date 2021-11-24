7bcb7e1cec383486e3654ea3ad64977253323419
Quincy Chang <quincy0710@hotmail.com>
2021-11-24 11:25:20 +0800

    feat(template): CWC-295 add coin-template getAddress

--- packages/coin-template/src/index.ts
+++ packages/coin-template/src/index.ts
@@ -1,82 +1,45 @@
-import { coin as COIN, setting, apdu } from '@coolwallet/core';
-import * as ethSign from './sign';
-import { pubKeyToAddress } from './utils/ethUtils';
-import * as types from './config/types';
-import * as scriptUtils from './utils/scriptUtils';
-import * as scriptUtilsEIP1559 from './utils/scriptUtilsEIP1559';
-import * as params from './config/params';
+import crypto from 'crypto';
+import {
+  coin as COIN, transport as Transport, utils, config
+} from '@coolwallet/core';
 
-type signTx = {
-  transport: Transport,
-  appPrivateKey: string,
-  appId: string,
-  transaction: Transaction,
-  addressIndex: number,
-  publicKey: string | undefined,
-  confirmCB: ()=>void | undefined,
-  authorizedCB: ()=>void | undefined
-}
+const bip32 = require('bip32');
+const elliptic = require('elliptic');
+const ec = new elliptic.ec('secp256k1');
+const createKeccakHash = require('keccak')
 
-type Transaction = {
-  chainId: number,
-  nonce: string,
-  gasPrice: string,
-  gasLimit: string,
-  to: string,
-  value: string,
-  data: string,
-}
-
-export default class TEMP extends COIN.ECDSACoin implements COIN.Coin {
-  constructor() {
-    super(params.COIN_TYPE);
-  }
-
-  async getAddress(
-    transport: types.Transport,
+export default class ADA implements COIN.Coin {
+  getAddress = async (
+    transport: Transport.default,
     appPrivateKey: string,
     appId: string,
     addressIndex: number
-  ): Promise<string> {
-    const publicKey = await this.getPublicKey(
-      transport, appPrivateKey, appId, addressIndex
-    );
-    return pubKeyToAddress(publicKey);
+  ): Promise<string> => {
+    const path = await utils.getFullPath({
+      pathType: config.PathType.BIP32ED25519,
+      pathString: "44'/60'/0'",
+    });
+    const accExtKey = await COIN.getAccountExtKeyFromSE(transport, appId, appPrivateKey, path);
+    const accExtKeyBuf = Buffer.from(accExtKey, 'hex');
+    const accPublicKey = accExtKeyBuf.slice(0, 33);
+    const accChainCode = accExtKeyBuf.slice(33);
+
+    const accNode = bip32.fromPublicKey(accPublicKey, accChainCode);
+    const changeNode = accNode.derive(0);
+    const addressNode = changeNode.derive(0);
+    const publicKey = addressNode.publicKey.toString('hex');
+
+    const uncompressedKey = ec.keyFromPublic(publicKey, "hex").getPublic(false);
+    const keyHash = createKeccakHash('keccak256').update(uncompressedKey).digest('hex');
+    const address = "0x".concat(keyHash.substr(-40));
+    return address;
   }
 
-  async getAddressByAccountKey(
-    accPublicKey: string,
-    accChainCode: string,
-    addressIndex: number
-  ): Promise<string> {
-    const publicKey = await this.getAddressPublicKey(
-      accPublicKey, accChainCode, addressIndex
-    );
-    return pubKeyToAddress(publicKey);
-  }
+  signTransaction = async (
+  ): Promise<string> => {
 
-  async signTransaction(
-    signTxData: signTx
-  ): Promise<string> {
-    const { value, data, to } = signTxData.transaction;
+    const signedTransaction = '';
 
-    const {
-      transport, appPrivateKey, appId, addressIndex, transaction
-    } = signTxData;
-
-    const publicKey = await this.getPublicKey(
-      transport, appPrivateKey, appId, addressIndex
-    );
-    const argument = await scriptUtils.getSmartContractArgument(
-      transaction, addressIndex
-    );
-    const script = params.SmartContract.scriptWithSignature;
-
-    return ethSign.signTransaction(
-      signTxData,
-      script,
-      argument,
-      publicKey,
-    );
-  }
+    return signedTransaction;
+  };
 }
