b9e6a1659e8b48cffc922d029fda11d709b1153b
Hank Liu <kccc418@gmail.com>
2021-12-03 17:57:36 +0800

    update XLM README and sign parameter

--- packages/coin-xlm/src/sign.ts
+++ packages/coin-xlm/src/sign.ts
@@ -2,7 +2,6 @@ import { tx, apdu, utils } from '@coolwallet/core';
 import * as scriptUtil from './utils/scriptUtil';
 import { signTxType, PROTOCOL } from './config/types';
 
-
 // const accountIndexToKeyId = (coinType: string, accountIndex: number) => {
 //   const accountIndexHex = accountIndex.toString(16).padStart(2, '0');
 //   const keyId = coinType.concat(accountIndexHex).concat('000000');
@@ -11,11 +10,10 @@ import { signTxType, PROTOCOL } from './config/types';
 
 export default async function signTransaction(
   signTxData: signTxType,
-  transfer: { script: string, signature: string },
-  protocol: PROTOCOL,
-): Promise<{ r: string; s: string; } | Buffer> {
-
-  const { transaction, transport, appPrivateKey, appId, signatureBase, confirmCB, authorizedCB } = signTxData
+  transfer: { script: string; signature: string },
+  protocol: PROTOCOL
+): Promise<{ r: string; s: string } | Buffer> {
+  const { transaction, transport, appPrivateKey, appId, confirmCB, authorizedCB } = signTxData;
 
   const preActions = [];
 
@@ -23,17 +21,12 @@ export default async function signTransaction(
 
   const sendScript = async () => {
     await apdu.tx.sendScript(transport, script);
-  }
+  };
   preActions.push(sendScript);
 
   const sendArgument = async () => {
-    return await apdu.tx.executeScript(
-      transport,
-      appId,
-      appPrivateKey,
-      argument
-    );
-  }
+    return await apdu.tx.executeScript(transport, appId, appPrivateKey, argument);
+  };
 
   const signature = await tx.flow.getSingleSignatureFromCoolWallet(
     transport,
@@ -42,10 +35,9 @@ export default async function signTransaction(
     true,
     confirmCB,
     authorizedCB,
-    false,
+    false
   );
   await utils.checkSupportScripts(transport);
-
 
   return signature;
 }
