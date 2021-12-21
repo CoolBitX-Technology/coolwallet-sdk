463ec0af0634df4a29ed3206614aaab9194b3329
srt1678 <chensteve520@gmail.com>
2021-12-21 10:48:50 +0800

    fix_eth_unit_test

--- packages/coin-eth/tests/sign.test.js
+++ packages/coin-eth/tests/sign.test.js
@@ -1,15 +1,12 @@
 /* eslint-disable max-len */
 /* eslint-disable class-methods-use-this */
+jest.mock('@coolwallet/core');
 import ETH from '../src';
 
 const appId = '0533ac5f177331221642adc02f7265bc3edb84d1';
-// const appPublicKey = '04cc68637846686549886133daa6acc4dceeed546bc04b8f258211df59ba2e28bbb5d4a1384a920acf0bbd1654dbcb5431ea1629e9856551bccb425c641de9f801';
 const appPrivateKey = '57a1c4ecdfc2dad7d392bf0f707ddd7280623f1b7de21e5efd42895b4b178737';
-
-const coreModule = require('@coolwallet/core');
-
+const addressIndex = 0;
 class Transport {}
-
 const transport = new Transport();
 const eth = new ETH(transport, appPrivateKey, appId);
 
@@ -18,22 +15,23 @@ test('should get address ', async () => {
   expect(address).toBe('0xbAF99eD5b5663329FA417953007AFCC60f06F781');
 });
 
-test('should sign transaction ', async () => {
+test('should sign transaction with script command ', async () => {
   const transaction = {
     chainId: 1,
     nonce: '0x31b',
-    gasPrice: '0xb2d05e00',
+    gasPrice: '0x12a05f200',
     gasLimit: '0x520c',
     to: '0x0644De2A0Cf3f11Ef6Ad89C264585406Ea346a96',
     value: '0xde0b6b3a7640000',
-    data: '0x00',
+    data: '0x00'
   };
-  const tx = await eth.signTransaction(transaction, 0);
-  expect(tx).toBe('0xf86d82031b84b2d05e0082520c940644de2a0cf3f11ef6ad89c264585406ea346a96880de0b6b3a76400008025a0ac1feeae6a0d9c0b6e23152432a270889495dcfc837db978f10000d38102be02a01570ac1cea178b10c2bc4d1693ba6599c80679eac83c2548234a3ad8e4f117a4');
-});
-
-test('should call old generation functions ', async () => {
-  expect(coreModule.core.flow.prepareSEData).toHaveBeenCalledTimes(1);
-  expect(coreModule.core.flow.sendDataToCoolWallet).toHaveBeenCalledTimes(1);
-  expect(coreModule.core.flow.sendScriptAndDataToCard).toHaveBeenCalledTimes(0);
+  const signTxData = {
+    transport,
+    appPrivateKey,
+    appId,
+    transaction,
+    addressIndex
+  }
+  const tx = await eth.signTransaction(signTxData);
+  expect(tx).toBe('0xf86e82031b85012a05f20082520c940644de2a0cf3f11ef6ad89c264585406ea346a96880de0b6b3a76400008025a07cce23b352f3c1f11ef4833e76b3b0cb14ca17bb0097d197b307690a551d19eea0156703269448e84d2a82e07531375896fd6fc6e0478cdda876315611d4cad697');
 });
