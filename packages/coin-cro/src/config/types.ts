ce65d29493771b3bf7b0541e7258e84bcdc2479f
Hank Liu <kccc418@gmail.com>
2022-01-19 19:48:47 +0800

    add cronos transfer

---
+++ packages/coin-cro/src/config/types.ts
@@ -0,0 +1,82 @@
+import { Transport } from '@coolwallet/core';
+
+export type Output = {
+  address: string;
+  value: number;
+};
+
+export type Option = {
+  info: {
+    symbol: string;
+    decimals: string;
+  };
+};
+
+export type Transaction = {
+  nonce: string;
+  gasPrice: string;
+  gasLimit: string;
+  to: string;
+  value: string;
+  data: string;
+  option: Option;
+};
+
+export type signTx = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  transaction: Transaction;
+  addressIndex: number;
+  publicKey: string | undefined;
+  confirmCB: () => void | undefined;
+  authorizedCB: () => void | undefined;
+};
+
+export type signMsg = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  message: string;
+  addressIndex: number;
+  confirmCB: () => void | undefined;
+  authorizedCB: () => void | undefined;
+};
+
+export type signTyped = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  typedData: any;
+  addressIndex: number;
+  confirmCB: () => void | undefined;
+  authorizedCB: () => void | undefined;
+};
+
+export const EIP712Schema = {
+  type: 'object',
+  properties: {
+    types: {
+      type: 'object',
+      properties: {
+        EIP712Domain: { type: 'array' },
+      },
+      additionalProperties: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            type: { type: 'string' },
+          },
+          required: ['name', 'type'],
+        },
+      },
+      required: ['EIP712Domain'],
+    },
+    primaryType: { type: 'string' },
+    domain: { type: 'object' },
+    message: { type: 'object' },
+  },
+  required: ['types', 'primaryType', 'domain', 'message'],
+};
--- packages/coin-cro/src/index.ts
+++ packages/coin-cro/src/index.ts
@@ -1,23 +1,88 @@
-import { coin as COIN, Transport, utils, config, apdu, tx } from '@coolwallet/core';
+import { coin as COIN, Transport } from '@coolwallet/core';
+import * as ethSign from './sign';
 import { pubKeyToAddress } from './utils/ethUtils';
 import * as params from './config/params';
+import * as types from './config/types';
+import * as scriptUtils from './utils/scriptUtils';
+import { TOKENTYPE } from './config/tokenType';
 
-export default class CRO extends COIN.ECDSACoin implements COIN.Coin{
-    constructor(){
-        super(params.COIN_TYPE);
+export default class CRO extends COIN.ECDSACoin implements COIN.Coin {
+  constructor() {
+    super(params.COIN_TYPE);
+  }
+
+  async getAddress(transport: Transport, appPrivateKey: string, appId: string, addressIndex: number): Promise<string> {
+    const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
+    return pubKeyToAddress(publicKey);
+  }
+
+  getAddressByAccountKey(accPublicKey: string, accChainCode: string, addressIndex: number): string {
+    const publicKey = this.getAddressPublicKey(accPublicKey, accChainCode, addressIndex);
+    return pubKeyToAddress(publicKey);
+  }
+
+  async signTransaction(signTxData: types.signTx): Promise<string> {
+    const { value, data, to } = signTxData.transaction;
+    // eth
+    if (value && !data) {
+      return this.signTransferTransaction(signTxData);
     }
 
-    async getAddress(transport: Transport, appPrivateKey: string, appId: string, addressIndex: number): Promise<string> {
-        const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
-        return pubKeyToAddress(publicKey);
-    };
+    // erc20
+    const functionHash = data.startsWith('0x') ? data.slice(2, 10) : data.slice(0, 8);
 
-    async getAddressByAccountKey(accPublicKey: string, accChainCode: string, addressIndex: number): Promise<string> {
-        const publicKey = await this.getAddressPublicKey(accPublicKey, accChainCode, addressIndex);
-        return pubKeyToAddress(publicKey);
+    if (data && functionHash === 'a9059cbb') {
+      const upperCaseAddress = to.toUpperCase(); // contractAddr
+      let tokenSignature;
+      for (const tokenInfo of TOKENTYPE) {
+        // get tokenSignature
+        if (tokenInfo.contractAddress.toUpperCase() === upperCaseAddress) {
+          tokenSignature = tokenInfo.signature;
+          signTxData.transaction.option.info.symbol = tokenInfo.symbol;
+          signTxData.transaction.option.info.decimals = tokenInfo.unit;
+          break;
+        }
       }
 
-    signTransaction = async(): Promise<string> => {
-        return '';
+      const { symbol, decimals } = signTxData.transaction.option.info;
+      if (symbol && decimals) {
+        if (tokenSignature) {
+          // 內建
+          return this.signERC20Transaction(signTxData, tokenSignature);
+        }
+        // 自建
+        return this.signERC20Transaction(signTxData);
+      }
     }
+
+    // smart contract
+    return this.signSmartContractTransaction(signTxData);
+  }
+
+  async signTransferTransaction(signTxData: types.signTx): Promise<string> {
+    const { transport, appPrivateKey, appId, addressIndex, transaction } = signTxData;
+    const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
+    const argument = await scriptUtils.getTransferArgument(transaction, addressIndex);
+    const script = params.TRANSFER.scriptWithSignature;
+
+    return ethSign.signTransaction(signTxData, script, argument, publicKey);
+  }
+
+  async signERC20Transaction(signTxData: types.signTx, tokenSignature = ''): Promise<string> {
+    const { transport, appPrivateKey, appId, addressIndex, transaction } = signTxData;
+    const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
+    const argument = await scriptUtils.getERC20Argument(transaction, tokenSignature, addressIndex);
+    const script = params.ERC20.scriptWithSignature;
+
+    return ethSign.signTransaction(signTxData, script, argument, publicKey);
+  }
+
+  async signSmartContractTransaction(signTxData: types.signTx): Promise<string> {
+    const { transport, appPrivateKey, appId, addressIndex, transaction } = signTxData;
+    const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
+    const argument = await scriptUtils.getSmartContractArgument(transaction, addressIndex);
+    const script = params.SmartContract.scriptWithSignature;
+
+    return ethSign.signSmartContractTransaction(signTxData, script, argument, publicKey);
+  }
 }
