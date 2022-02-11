d0bb6004eb636491aa12b2384f0ff431e858caa8
hank121314 <hank121314@gmail.com>
2022-02-11 15:39:11 +0800

    feat(CRO): CWC-272 add Cronos smart contract

---
+++ packages/coin-tester/src/components/Inputs.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { memo, FC } from 'react';
+import { Row, Col, ButtonGroup, Button as BootStrapButton, Form } from 'react-bootstrap';
+import './Inputs.css'
+
+const row = 'inputs-component';
+
+const textArea = 'text-area';
+
+const inputCol = 'input-col';
+
+const buttonGroup = 'button-group';
+
+const button = 'button';
+
+interface Input {
+  xs?: number;
+  value: string;
+  placeholder: string;
+  onChange(value: string): void;
+}
+
+interface Props {
+  title: string;
+  inputs: Input[];
+  content: string;
+  onClick(): void;
+  btnTitle?: string;
+  variant?: string;
+  disabled?: boolean;
+}
+
+const ButtonInputs: FC<Props> = (props: Props) => {
+  return (
+    <Row className={row}>
+      <Col xs={2}>{props.title}</Col>
+      {props.inputs.map((input, i) => (
+        <Col xs={input.xs ?? 2} key={'' + i + ''} className={inputCol}>
+          <Form.Control
+            value={input.value}
+            onChange={(event) => {
+              input.onChange(event.target.value);
+            }}
+            placeholder={input.placeholder}
+          />
+        </Col>
+      ))}
+      <Col className={textArea}>{props.content}</Col>
+      <Col xs={2}>
+        <ButtonGroup className={buttonGroup}>
+          <BootStrapButton className={button} variant={props.variant} disabled={props.disabled} onClick={props.onClick}>
+            {props.btnTitle}
+          </BootStrapButton>
+        </ButtonGroup>
+      </Col>
+    </Row>
+  );
+};
+
+ButtonInputs.defaultProps = { disabled: false, variant: 'outline-light', btnTitle: 'Get' };
+
+export default memo(ButtonInputs);
--- packages/coin-tester/src/components/coins/coin-cro/index.tsx
+++ packages/coin-tester/src/components/coins/coin-cro/index.tsx
@@ -6,6 +6,7 @@ import Web3 from 'web3';
 
 import CRO from '@coolwallet/coin-cro';
 import { Transaction } from '@coolwallet/coin-cro/lib/config/types';
+import Inputs from '../../Inputs';
 
 const web3 = new Web3('https://evm-cronos.crypto.org');
 
@@ -23,6 +24,8 @@ function CoinCRO(props: Props) {
   const [signedTransaction, setSignedTransaction] = useState('');
   const [value, setValue] = useState('0');
   const [to, setTo] = useState('0x81bb32e4A7e4d0500d11A52F3a5F60c9A6Ef126C');
+  const [smartContractTo, setSmartContractTo] = useState('0x81bb32e4A7e4d0500d11A52F3a5F60c9A6Ef126C');
+  const [smartContractSignature, setSmartContractSignature] = useState('');
   const [data, setData] = useState('');
   const { transport, appPrivateKey } = props;
   const disabled = !transport || props.isLocked;
@@ -44,16 +47,14 @@ function CoinCRO(props: Props) {
     handleState(async () => {
       const appId = localStorage.getItem('appId');
       if (!appId) throw new Error('No Appid stored, please register!');
-      const address = await cro.getAddress(transport!, appPrivateKey, appId, 0);
-      return address;
+      return cro.getAddress(transport!, appPrivateKey, appId, 0);
     }, setAddress);
   };
 
   const signTransaction = async () => {
     handleState(async () => {
       const transaction: Transaction = {
-        nonce: web3.utils.toHex(
-          await web3.eth.getTransactionCount(address, 'pending')),
+        nonce: web3.utils.toHex(await web3.eth.getTransactionCount(address, 'pending')),
         gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
         gasLimit: web3.utils.toHex(await web3.eth.estimateGas({ to, data })),
         to: to,
@@ -85,27 +86,82 @@ function CoinCRO(props: Props) {
     }, setSignedTransaction);
   };
 
+  const signSmartContract = async () => {
+    handleState(async () => {
+      const transactionData = `0x${data}`;
+      const transaction = {
+        nonce: web3.utils.toHex(await web3.eth.getTransactionCount(address, 'pending')),
+        gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
+        gasLimit: web3.utils.toHex(await web3.eth.estimateGas({ to, data: transactionData })),
+        to: smartContractTo,
+        value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
+        data: transactionData,
+      } as Transaction;
+      console.log(transaction);
+
+      const appId = localStorage.getItem('appId');
+      if (!appId) throw new Error('No Appid stored, please register!');
+      const option = {
+        info: { symbol: '', decimals: '' },
+      };
+
+      const signTxData = {
+        transport: transport!,
+        appPrivateKey,
+        appId,
+        transaction: transaction,
+        addressIndex: 0,
+        option,
+      };
+
+      const signedTx = await cro.signSmartContractTransaction(signTxData);
+      console.log('signedTx :', signedTx);
+
+      await web3.eth.sendSignedTransaction(signedTx);
+
+      return signedTx;
+    }, setSmartContractSignature);
+  };
+
   return (
     <Container>
-      <div className="title2">These two basic methods are required to implement in a coin sdk.</div>
-      <NoInput title="Get Address" content={address} onClick={getAddress} disabled={disabled} />
-      {
-        <TwoInputs
-          title="Sign Transaction"
-          content={signedTransaction}
-          onClick={signTransaction}
-          disabled={disabled}
-          btnName="Sign&Send"
-          value={value}
-          setValue={setValue}
-          placeholder="value"
-          inputSize={1}
-          value2={to}
-          setValue2={setTo}
-          placeholder2="to"
-          inputSize2={3}
-        />
-      }
+      <div className='title2'>These two basic methods are required to implement in a coin sdk.</div>
+      <NoInput title='Get Address' content={address} onClick={getAddress} disabled={disabled} />
+
+      <TwoInputs
+        title='Sign Transaction'
+        content={signedTransaction}
+        onClick={signTransaction}
+        disabled={disabled}
+        btnName='Sign&Send'
+        value={value}
+        setValue={setValue}
+        placeholder='value'
+        inputSize={1}
+        value2={to}
+        setValue2={setTo}
+        placeholder2='to'
+        inputSize2={3}
+      />
+      <Inputs
+        btnTitle='Sign'
+        title='Sign Smart Contract'
+        content={smartContractSignature}
+        onClick={signSmartContract}
+        disabled={disabled}
+        inputs={[
+          {
+            value: smartContractTo,
+            onChange: setSmartContractTo,
+            placeholder: 'to',
+          },
+          {
+            value: data,
+            onChange: setData,
+            placeholder: 'data arg',
+          },
+        ]}
+      />
     </Container>
   );
 }
