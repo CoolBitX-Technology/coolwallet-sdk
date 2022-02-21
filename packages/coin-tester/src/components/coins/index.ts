f0800443b5d110c0371f3dc0015a2b954ba9674d
Wei-Tsung Su <ellington.su@gmail.com>
2022-02-22 01:06:47 +0800

    feat(coin-tester): add XTZ SDK tester app

--- packages/coin-tester/src/components/coins/index.ts
+++ packages/coin-tester/src/components/coins/index.ts
@@ -1,8 +1,11 @@
 import template from './template';
 import ETC from './ETC';
 import cro from './coin-cro';
+import xtz from './coin-xtz';
+
 export default [
   { path: 'Template', Element: template },
   { path: 'ETC', Element: ETC },
   { path: 'CRO', Element: cro},
+  { path: 'XTZ', Element: xtz},
 ];
