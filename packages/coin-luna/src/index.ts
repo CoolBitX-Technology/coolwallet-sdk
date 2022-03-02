5a80617e19284de21fd63e789e60bd4b4f6269dc
srt1678 <chensteve520@gmail.com>
2022-03-02 14:47:54 +0800

    Luna normal transfer

--- packages/coin-luna/src/index.ts
+++ packages/coin-luna/src/index.ts
@@ -1,12 +1,17 @@
 import { coin as COIN, Transport } from '@coolwallet/core';
 import * as params from './config/params';
 import * as txUtil from './utils/transactionUtils';
+import * as types from './config/types';
+import { SDKError } from '@coolwallet/core/lib/error';
+import * as scriptUtil from './utils/scriptUtil';
+import * as sign from './sign';
 
 export default class LUNA extends COIN.ECDSACoin implements COIN.Coin{
     public Types: any;
 
     constructor(){
         super(params.COIN_TYPE);
+        this.Types = types;
     }
 
     async getAddress(transport: Transport, appPrivateKey: string, appId: string, addressIndex: number): Promise<string>{
@@ -19,7 +24,40 @@ export default class LUNA extends COIN.ECDSACoin implements COIN.Coin{
         return txUtil.publicKeyToAddress(publicKey);
     }
 
-    async signTransaction(){
-        return '';
+    async signTransaction(signData: types.SignDataType): Promise<string> {
+        const chainId = signData.transaction.chainId;
+        switch (chainId) {
+          case types.CHAIN_ID.LUNA:
+            return this.signLUNATransaction(signData);
+          default:
+            throw new SDKError(this.signTransaction.name, `not support input chainId: ${chainId}`);
+        }
+    }
+
+    async signLUNATransaction(signData: types.SignDataType): Promise<string> {
+        const { addressIndex } = signData;
+    
+        const publicKey = await this.getPublicKey(signData.transport, signData.appPrivateKey, signData.appId, addressIndex);
+    
+        let script;
+        let argument;
+        let genTx;
+        switch (signData.txType) {
+          case types.TX_TYPE.SEND:
+            script = params.TRANSFER.script + params.TRANSFER.signature;
+            argument = scriptUtil.getLunaSendArgement(publicKey, signData.transaction, addressIndex);
+            genTx = (signature: string) => {
+              return txUtil.getSendTx(signData.transaction, signature, publicKey);
+            };
+            break;
+          default:
+            throw new SDKError(this.signLUNATransaction.name, `not support input tx type`);
+        }
+        const signature = await sign.signTransaction(signData, script, argument);
+        console.debug('signature: ', signature);
+        const signTx = genTx(signature);
+        console.debug('signTx protobuf: ', signTx);
+        const txBytesBase64 = Buffer.from(signTx, 'hex').toString('base64');
+        return txBytesBase64;
     }
 }
