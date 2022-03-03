22961276127982bb6187e1ccb39d7eccf31fc4bc
Hank <59682758+hankliu418@users.noreply.github.com>
2022-03-03 14:55:30 +0800

    feat(coin-eth): CW-6231 Fix EIP1559 issue (#303)

--- packages/coin-eth/src/utils/ethUtilsEIP1559.ts
+++ packages/coin-eth/src/utils/ethUtilsEIP1559.ts
@@ -7,9 +7,7 @@ const elliptic = require('elliptic');
 // eslint-disable-next-line new-cap
 const ec = new elliptic.ec('secp256k1');
 
-export const getRawHex = (
-  transaction: EIP1559Transaction
-): Array<Buffer|Buffer[]> => {
+export const getRawHex = (transaction: EIP1559Transaction): Array<Buffer | Buffer[]> => {
   const rawData = [];
   rawData.push('01'); // chainId
   rawData.push(transaction.nonce);
@@ -19,7 +17,7 @@ export const getRawHex = (
   rawData.push(transaction.to);
   rawData.push(transaction.value);
   rawData.push(transaction.data);
-  const raw: Array<Buffer|Buffer[]> = rawData.map((d) => {
+  const raw: Array<Buffer | Buffer[]> = rawData.map((d) => {
     const hex = handleHex(d);
     if (hex === '00' || hex === '') {
       return Buffer.allocUnsafe(0);
@@ -32,15 +30,14 @@ export const getRawHex = (
   return raw;
 };
 
-export const composeSignedTransacton = (
-  payload: Array<Buffer|Buffer[]>, v: number, r: string, s: string
-): string => {
+export const composeSignedTransacton = (payload: Array<Buffer | Buffer[]>, v: number, r: string, s: string): string => {
   const transaction = payload;
-  transaction.push(
-    Buffer.from([v]),
-    Buffer.from(r, 'hex'),
-    Buffer.from(s, 'hex')
-  );
+  if (v === 0) {
+    transaction.push(Buffer.allocUnsafe(0));
+  } else {
+    transaction.push(Buffer.from([v]));
+  }
+  transaction.push(Buffer.from(r, 'hex'), Buffer.from(s, 'hex'));
   const serializedTx = rlp.encode(transaction);
   return `0x02${serializedTx.toString('hex')}`;
 };
@@ -49,18 +46,14 @@ export const genEthSigFromSESig = async (
   canonicalSignature: { r: string; s: string },
   payload: Buffer,
   compressedPubkey: string | undefined = undefined
-): Promise<{ v: number; r: string; s: string; }> => {
+): Promise<{ v: number; r: string; s: string }> => {
   const prefixedPayload = Buffer.concat([Buffer.from([2]), payload]);
   const hash = Web3.utils.keccak256(prefixedPayload);
   const data = Buffer.from(handleHex(hash), 'hex');
   const keyPair = ec.keyFromPublic(compressedPubkey, 'hex');
 
   // get v
-  const recoveryParam = ec.getKeyRecoveryParam(
-    data,
-    canonicalSignature,
-    keyPair.pub
-  );
+  const recoveryParam = ec.getKeyRecoveryParam(data, canonicalSignature, keyPair.pub);
   const v = recoveryParam;
   const { r } = canonicalSignature;
   const { s } = canonicalSignature;
