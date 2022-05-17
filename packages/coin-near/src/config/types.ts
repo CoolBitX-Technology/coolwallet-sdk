af3a05bf8c0ad15d9fb8d559a3c2d72d8b0e025b
Quincy Chang <quincy0710@hotmail.com>
2022-05-17 09:58:50 +0800

    fix(near): CW-7273 refactor near protocol (#419)

--- packages/coin-near/src/config/types.ts
+++ packages/coin-near/src/config/types.ts
@@ -1,6 +1,16 @@
 import { Transport } from '@coolwallet/core';
 export { Transport };
 
+export type Input =
+  | SignTransferTxType
+  | SignStakeTxType
+  | SignSmartTxType
+  | SignSCStakeTxType
+  | SignSCUnstakeAllTxType
+  | SignSCWithdrawAllTxType
+  | SignSCUnstakeTxType
+  | SignSCWithdrawTxType;
+
 // Transfer
 
 export type SignTransferTxType = {
@@ -10,16 +20,16 @@ export type SignTransferTxType = {
   transaction: TransferTxType;
   confirmCB?(): void;
   authorizedCB?(): void;
-}
+};
 
 export type TransferTxType = {
   sender?: string;
-  publicKey?: string; 
+  publicKey?: string;
   receiver: string;
   nonce: number;
   recentBlockHash: string;
   amount: string;
-}
+};
 
 // Stake
 
@@ -30,17 +40,17 @@ export type SignStakeTxType = {
   transaction: StakeTxType;
   confirmCB?(): void;
   authorizedCB?(): void;
-}
+};
 
 export type StakeTxType = {
   sender?: string;
-  publicKey?: string; 
+  publicKey?: string;
   receiver?: string;
   nonce: number;
   recentBlockHash: string;
   amount: string;
   validatorPublicKey: string;
-}
+};
 
 // Smart
 
@@ -51,11 +61,11 @@ export type SignSmartTxType = {
   transaction: SmartTxType;
   confirmCB?(): void;
   authorizedCB?(): void;
-}
+};
 
 export type SmartTxType = {
   sender?: string;
-  publicKey?: string; 
+  publicKey?: string;
   receiver: string;
   nonce: number;
   recentBlockHash: string;
@@ -63,7 +73,110 @@ export type SmartTxType = {
   gas?: string;
   methodName: string;
   methodArgs: Uint8Array;
-}
+};
+
+// SCStake
+
+export type SignSCStakeTxType = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  transaction: SCDelegateTxType;
+  confirmCB?(): void;
+  authorizedCB?(): void;
+};
+
+export type SCDelegateTxType = {
+  sender?: string;
+  publicKey?: string;
+  receiver: string;
+  nonce: number;
+  recentBlockHash: string;
+  amount: string;
+  gas: string;
+};
+
+// SCUnstakeAll
+
+export type SignSCUnstakeAllTxType = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  transaction: SCUnstakeAllTxType;
+  confirmCB?(): void;
+  authorizedCB?(): void;
+};
+
+export type SCUnstakeAllTxType = {
+  sender?: string;
+  publicKey?: string;
+  receiver: string;
+  nonce: number;
+  recentBlockHash: string;
+  gas: string;
+};
+
+// SCWithdrawAll
+
+export type SignSCWithdrawAllTxType = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  transaction: SCWithdrawAllTxType;
+  confirmCB?(): void;
+  authorizedCB?(): void;
+};
+
+export type SCWithdrawAllTxType = {
+  sender?: string;
+  publicKey?: string;
+  receiver: string;
+  nonce: number;
+  recentBlockHash: string;
+  gas: string;
+};
+
+// SCUnstake
+
+export type SignSCUnstakeTxType = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  transaction: SCUnstakeTxType;
+  confirmCB?(): void;
+  authorizedCB?(): void;
+};
+
+export type SCUnstakeTxType = {
+  sender?: string;
+  publicKey?: string;
+  receiver: string;
+  nonce: number;
+  recentBlockHash: string;
+  amount: string;
+  gas: string;
+};
+
+// SCWithdraw
+
+export type SignSCWithdrawTxType = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  transaction: SCWithdrawTxType;
+  confirmCB?(): void;
+  authorizedCB?(): void;
+};
+
+export type SCWithdrawTxType = {
+  sender?: string;
+  publicKey?: string;
+  receiver: string;
+  nonce: number;
+  recentBlockHash: string;
+  amount: string;
+  gas: string;
+};
 
 // General
 
@@ -74,16 +187,16 @@ export type SignTxType = {
   transaction: TransactionType;
   confirmCB?(): void;
   authorizedCB?(): void;
-}
+};
 
 export type TransactionType = {
   sender?: string;
-  publicKey?: string; 
+  publicKey?: string;
   receiver?: string;
   nonce: number;
   recentBlockHash: string;
   action: Action;
-}
+};
 
 export type Action = {
   txnType: TxnType;
@@ -94,11 +207,15 @@ export type Action = {
   gas?: string;
   methodName?: string;
   methodArgs?: Uint8Array;
-}
+};
 
 export enum TxnType {
   TRANSFER = 1,
   STAKE = 2,
   SMART = 3,
-  SCSTAKE = 4
+  SCStake = 4,
+  SCUnstakeAll = 5,
+  SCWithdrawAll = 6,
+  SCUnstake = 7,
+  SCWithdraw = 8,
 }
