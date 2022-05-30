b77d4bfaeaad083508766cac75fdc823e3b655f3
kanitsu <93117687+kanitsu@users.noreply.github.com>
2022-05-30 15:20:31 +0800

    Add Celo signature

--- packages/coin-evm/src/chain/celo/index.ts
+++ packages/coin-evm/src/chain/celo/index.ts
@@ -4,7 +4,11 @@ import { ChainProps } from '../types';
 class CeloChain extends ChainProps {
   id = 42220;
   symbol = 'CELO';
-  signature = `FA`.padEnd(144, '0');
+  signature =
+    `30460221008CB3981C9663D04CE5377781224F3D89098BCBC998E4F2BF24B3A7B6D729AAC4022100C6BAE8CC5956FA298B05F50A05E28DD6D18CC8990483FBCECB3E807339AC4D80`.padStart(
+      144,
+      '0'
+    );
   tokens = TOKENS;
 }
 
@@ -17,4 +21,3 @@ class CeloTestChain extends ChainProps {
 
 export const CELO = new CeloChain();
 export const CELO_TEST = new CeloTestChain();
-
