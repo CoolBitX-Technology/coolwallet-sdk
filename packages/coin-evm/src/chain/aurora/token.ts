ec632e39fd0976b7951884a56cea21cf39bb8cb0
Eddie Chien <eddie71677@gmail.com>
2022-06-09 02:49:26 +0800

    add tokens in avaxc & aurora chain

--- packages/coin-evm/src/chain/aurora/token.ts
+++ packages/coin-evm/src/chain/aurora/token.ts
@@ -1,3 +1,68 @@
-export const TOKENS = {};
+export const TOKENS = {
+  USDT: {
+    name: 'Tether USD',
+    symbol: 'USDT',
+    unit: '6',
+    contractAddress: '0x4988a896b1227218e4A686fdE5EabdcAbd91571f',
+    signature: '',
+  },
+  USDC: {
+    name: 'USD Coin',
+    symbol: 'USDT',
+    unit: '6',
+    contractAddress: '0xB12BFcA5A55806AaF64E99521918A4bf0fC40802',
+    signature: '',
+  },
+  WETH: {
+    name: 'Wrapped Ether',
+    symbol: 'WETH',
+    unit: '18',
+    contractAddress: '0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB',
+    signature: '',
+  },
+  FRAX: {
+    name: 'Frax',
+    symbol: 'FRAX',
+    unit: '18',
+    contractAddress: '0xE4B9e004389d91e4134a28F19BD833cBA1d994B6',
+    signature: '',
+  },
+  FXS: {
+    name: 'Frax Share',
+    symbol: 'FXS',
+    unit: '18',
+    contractAddress: '0xBb8831701E68B99616bF940b7DafBeb4CDb23e0b',
+    signature: '',
+  },
+  AURORA: {
+    name: 'Aurora',
+    symbol: 'AURORA',
+    unit: '18',
+    contractAddress: '0x8BEc47865aDe3B172A928df8f990Bc7f2A3b9f79',
+    signature: '',
+  },
+  XNL: {
+    name: 'Chronicle',
+    symbol: 'XNL',
+    unit: '18',
+    contractAddress: '0x7cA1C28663b76CFDe424A9494555B94846205585',
+    signature: '',
+  },
+  WANNA: {
+    name: 'WannaSwap',
+    symbol: 'WANNA',
+    unit: '18',
+    contractAddress: '0x7faA64Faf54750a2E3eE621166635fEAF406Ab22',
+    signature: '',
+  },
+};
 
-export const TEST_TOKENS = {};
+export const TEST_TOKENS = {
+  MTK: {
+    name: 'MyToken',
+    symbol: 'MTK',
+    unit: '18',
+    contractAddress: '0x49D728c678d8eb2A94A23b1EDb59FdC07a7497dC',
+    signature: '',
+  },
+};
