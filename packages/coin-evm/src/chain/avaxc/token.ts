6e86f55cb6219ea5162ca381756a60e8f636fb19
Eddie Chien <eddie71677@gmail.com>
2022-06-08 17:17:21 +0800

    add avaxc & aurora testnet

--- packages/coin-evm/src/chain/avaxc/token.ts
+++ packages/coin-evm/src/chain/avaxc/token.ts
@@ -1,1 +1,11 @@
 export const TOKENS = {};
+
+export const TEST_TOKENS = {
+  LINK: {
+    name: 'ChainLink Token',
+    symbol: 'LINK',
+    unit: '18',
+    contractAddress: '0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846',
+    signature: '',
+  },
+};
