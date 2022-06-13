08cb0649c2392f90afe6834e7d6247682037405c
srt1678 <chensteve520@gmail.com>
2022-06-13 16:48:16 +0800

    minor adjustment

--- packages/coin-xtz/src/config/tokenType.ts
+++ packages/coin-xtz/src/config/tokenType.ts
@@ -1,12 +1,11 @@
 export const TOKENTYPE = [
   {
-    // <Example, Official Tokens TBD>
-    // name: 'Kolibri USD',
-    // symbol: 'kUSD',
-    // unit: '18',
-    // token_id: '0',
-    // contractAddress: 'KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV',
-    // signature:
-    //   '12046B5553440000004B54314B3967435267614C52464B544572597431775678413346726239466A61736A5456' + 'FA'.padEnd(144, '0'),
+    // <Official Tokens TBD>
+    name: 'Temp',
+    symbol: 'Temp',
+    unit: 'Temp',
+    token_id: '0',
+    contractAddress: 'Temp',
+    signature: 'FA'.padEnd(144, '0')
   }
 ];
