b3c3ab70e48d4af9fb420bb3859711ea5c5b8e98
kanitsu <93117687+kanitsu@users.noreply.github.com>
2022-06-24 14:27:23 +0800

    Add Polygon ERC-20

--- packages/coin-evm/src/chain/polygon/token.ts
+++ packages/coin-evm/src/chain/polygon/token.ts
@@ -1,16 +1,73 @@
 export const TOKENS = {
-  WETH: {
-    name: 'Wrapped Ether',
-    symbol: 'WETH',
-    unit: '18',
-    contractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
-    signature: '',
-  },
   USDT: {
     name: 'Tether USD',
     symbol: 'USDT',
     unit: '6',
     contractAddress: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
+    signature:
+      '060455534454000000c2132d05d31c914a87c6611c10748aeb04b58e8f3045022100FDD2F1A2261F545063E8A5A07D96C6584B870DF184F7F0B2057B1AC98E3246150220061F92EB8AAC6BD120659A2D3E0184456135998F0B68224CBE9E752BA2BEAD85',
+  },
+  USDC: {
+    name: 'USD Coin',
+    symbol: 'USDC',
+    unit: '6',
+    contractAddress: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+    signature:
+      '0604555344430000002791bca1f2de4661ed88a30c99a7a9449aa84174304602210080FAD84D06FCCFA30914A2421CB06745421E0ADC93E4F5BE04E49357BED13214022100ADE6694BFC49FF23996FECBDC6CB8DD21050B4EC409A58961AA9204A52CE7B94',
+  },
+  BUSD: {
+    name: 'binance-usd',
+    symbol: 'BUSD',
+    unit: '18',
+    contractAddress: '0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7',
+    signature:
+      '120442555344000000dab529f40e671a1d4bf91361c21bf9f0c9712ab7304402205AA7F3DE572DDFDEE0CD5296D0008749C3F0EC956A9C1EA15AC9B865AD6C0FAB02205B049B3D24D586086F42397E97C15B41363791F05E29B01C011C8BD54A439A8C',
+  },
+  DAI: {
+    name: 'Dai Stablecoin',
+    symbol: 'DAI',
+    unit: '18',
+    contractAddress: '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063',
+    signature:
+      '1203444149000000008f3cf7ad23cd3cadbd9735aff958023239c6a0633046022100890B702278F97AA34F8EA6ECA2F36A754B4DA5F804F58506B929E9B82152C993022100CF8E08D394CAD84193DAF2993AFDB220068D29D962F1399DFFE16D6F2AACE77E',
+  },
+  WBTC: {
+    name: 'Wrapped BTC',
+    symbol: 'WBTC',
+    unit: '8',
+    contractAddress: '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6',
+    signature:
+      '0804574254430000001bfd67037b42cf73acf2047067bd4f2c47d9bfd6304502204FDACF27D1F85F0CD868C1DD71576AB064D76C3E4C91A71C2CE3C8A20B3E07B9022100A1ADBE88FC285F6AC9B35520A2FB71FC39A8420CBBEED039E903B12D856954BF',
+  },
+  AVAX: {
+    name: 'Avalanche Token',
+    symbol: 'AVAX',
+    unit: '18',
+    contractAddress: '0x2c89bbc92bd86f8075d1decc58c7f4e0107f286b',
+    signature:
+      '1204415641580000002c89bbc92bd86f8075d1decc58c7f4e0107f286b304502202A79900887E87ED9FCAE5D8B888A39A0F37318234E926C4526638DD29DFE8C05022100DEC13ABF3CAE4727711D6041BC8246FC325B5077E22C6D78BACDC08FA12528B9',
+  },
+  LINK: {
+    name: 'ChainLink Token',
+    symbol: 'LINK',
+    unit: '18',
+    contractAddress: '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39',
+    signature:
+      '12044c494e4b00000053e0bca35ec356bd5dddfebbd1fc0fd03fabad3930450220298EDC2D2CE658A746C54D54166161F988B53B810ACF3E7F67B4694A696515F7022100955A98E796BBA6FAFE37A4AFB76A550A2DDB597D434CC7DD4441A4F11285BC93',
+  },
+  UNI: {
+    name: 'Uniswap',
+    symbol: 'UNI',
+    unit: '18',
+    contractAddress: '0xb33eaad8d922b1083446dc23f610c2567fb5180f',
+    signature:
+      '1203554e4900000000b33eaad8d922b1083446dc23f610c2567fb5180f3045022100994A26D883D4E2731591E19878CCEDF4F5A42A450D35338AD6139147C90D347202200979A5D2D6F64111F6E1C329CEAA13E96C0009D655B4404E99C5C4B9EE65430B',
+  },
+  WETH: {
+    name: 'Wrapped Ether',
+    symbol: 'WETH',
+    unit: '18',
+    contractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
     signature: '',
   },
   BNB: {
@@ -20,32 +77,11 @@ export const TOKENS = {
     contractAddress: '0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3',
     signature: '',
   },
-  USDC: {
-    name: 'USD Coin',
-    symbol: 'USDC',
-    unit: '6',
-    contractAddress: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
-    signature: '',
-  },
   MATIC: {
     name: 'Matic Token',
     symbol: 'MATIC',
     unit: '18',
     contractAddress: '0x0000000000000000000000000000000000001010',
-    signature: '',
-  },
-  BUSD: {
-    name: 'binance-usd',
-    symbol: 'BUSD',
-    unit: '18',
-    contractAddress: '0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7',
-    signature: '',
-  },
-  WBTC: {
-    name: 'Wrapped BTC',
-    symbol: 'WBTC',
-    unit: '8',
-    contractAddress: '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6',
     signature: '',
   },
   CRO: {
@@ -60,13 +96,6 @@ export const TOKENS = {
     symbol: 'UST',
     unit: '18',
     contractAddress: '0x692597b009d13c4049a947cab2239b7d6517875f',
-    signature: '',
-  },
-  LINK: {
-    name: 'ChainLink Token',
-    symbol: 'LINK',
-    unit: '18',
-    contractAddress: '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39',
     signature: '',
   },
 };
