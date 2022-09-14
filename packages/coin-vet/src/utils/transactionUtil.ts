4cb7143bceeba419ff190d53725414f219847a5c
leongchien-wits <ahkoh.dev@gmail.com>
2022-09-14 22:38:49 +0800

    add chainTag to rawtx

--- packages/coin-vet/src/utils/transactionUtil.ts
+++ packages/coin-vet/src/utils/transactionUtil.ts
@@ -32,6 +32,7 @@ export function pubKeyToAddress(compressedPubkey: string): string {
 export const getRawTx = (transaction: types.Record): any => {
   const raw = []
   const rawData1 = [];
+  rawData1.push('4a');
   rawData1.push(stringUtil.handleHex(transaction.blockRef).replace(/^0+/, ''));
   rawData1.push(transaction.expiration);
   rawData1.map((d) => {
