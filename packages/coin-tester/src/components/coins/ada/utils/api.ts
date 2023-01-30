5847f93ea3fc1528f43f0b560cc039d588e90bfd
kanitsu <93117687+kanitsu@users.noreply.github.com>
2023-01-30 15:51:58 +0800

    Add testnet API and config for easy switch

--- packages/coin-tester/src/components/coins/ada/utils/api.ts
+++ packages/coin-tester/src/components/coins/ada/utils/api.ts
@@ -1,37 +1,44 @@
 import 'isomorphic-fetch';
 
-const project_id = 'mainnetEMA6xeDs6p9hq2Cxfs9hCVPfRmjpSEu1';
+const config = {
+  project_id: 'mainnetEMA6xeDs6p9hq2Cxfs9hCVPfRmjpSEu1',
+  network: 'mainnet',
+};
+// const config = {
+//   project_id: 'preprod5wKKMffQzylz8tdSR56Q3jdiwymVLjag',
+//   network: 'preprod',
+// };
 
 async function getData(url: string) {
   const response = await fetch(url, {
     method: 'GET',
-    headers: { project_id }
+    headers: { project_id: config.project_id }
   });
   const result = await response.json();
   return result;
 }
 
 export async function getAddressInfo(address: string) {
-  return getData(`https://cardano-mainnet.blockfrost.io/api/v0/addresses/${address}`);
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/addresses/${address}`);
 }
 
 export async function getLatestBlock() {
-  return getData('https://cardano-mainnet.blockfrost.io/api/v0/blocks/latest');
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/blocks/latest`);
 }
 
 export async function getLatestProtocolParameters() {
-  return getData('https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest/parameters');
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/epochs/latest/parameters`);
 }
 
 export async function getUtxos(address: string) {
-  return getData(`https://cardano-mainnet.blockfrost.io/api/v0/addresses/${address}/utxos`);
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/addresses/${address}/utxos`);
 }
 
 export async function sendTx(tx: string) {
-  const url = 'https://cardano-mainnet.blockfrost.io/api/v0/tx/submit';
+  const url = `https://cardano-${config.network}.blockfrost.io/api/v0/tx/submit`;
   const response = await fetch(url, {
     method: 'POST',
-    headers: { project_id, 'Content-Type': 'application/cbor' },
+    headers: { project_id: config.project_id , 'Content-Type': 'application/cbor' },
     body: Buffer.from(tx, 'hex'),
   });
   console.log('response :', response);
@@ -44,17 +51,17 @@ export async function sendTx(tx: string) {
 // Staking
 
 export async function getStakePools() {
-  return getData('https://cardano-mainnet.blockfrost.io/api/v0/pools?count=10');
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/pools?count=10`);
 }
 
 export async function getRegistrationHistory(stakeAddress: string) {
-  return getData(`https://cardano-mainnet.blockfrost.io/api/v0/accounts/${stakeAddress}/registrations`);
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/accounts/${stakeAddress}/registrations`);
 }
 
 export async function getDelegationHistory(stakeAddress: string) {
-  return getData(`https://cardano-mainnet.blockfrost.io/api/v0/accounts/${stakeAddress}/delegations`);
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/accounts/${stakeAddress}/delegations`);
 }
 
 export async function getAccountInfo(stakeAddress: string) {
-  return getData(`https://cardano-mainnet.blockfrost.io/api/v0/accounts/${stakeAddress}`);
+  return getData(`https://cardano-${config.network}.blockfrost.io/api/v0/accounts/${stakeAddress}`);
 }
