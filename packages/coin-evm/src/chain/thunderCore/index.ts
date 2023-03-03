fe750b52bbe95bfeb678fb5f944778d86991ba06
kanitsu <93117687+kanitsu@users.noreply.github.com>
2023-03-03 10:37:49 +0800

    Add ThunderCore signature

--- packages/coin-evm/src/chain/thunderCore/index.ts
+++ packages/coin-evm/src/chain/thunderCore/index.ts
@@ -4,7 +4,11 @@ import { ChainProps } from '../types';
 class ThunderCore extends ChainProps {
   id = 108;
   symbol = 'TT';
-  signature = `FA`.padEnd(144, '0');
+  signature =
+    `3045022100B342B65121F2CB9623BD947EEC1373AB73478D115242BB262F90DC5573C98B2402203D5AC87C64974078D503B11335E6E45CD75A85A9E94E92773FFE118D1C8A2893`.padStart(
+      144,
+      '0'
+    );
   tokens = TOKENS;
 }
 
