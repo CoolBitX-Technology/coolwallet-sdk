28382eff8938420da306dc1e55694e8ad8885a64
Brian Yeh <brian.yeh@coolbitx.com>
2023-06-14 18:09:42 +0800

    feat: update GALA ERC-20 token contract address

--- packages/coin-evm/tools/token.ts
+++ packages/coin-evm/tools/token.ts
@@ -1,7 +1,8 @@
 import { OKX, ZKSYNC } from '../src/chain';
 import { TokenProps } from '../src/chain/types';
 
-const buildTokenHex = (token: TokenProps) => {
+type BuildTokenHexInput = Pick<TokenProps, 'symbol' | 'unit' | 'contractAddress'>;
+const buildTokenHex = (token: BuildTokenHexInput) => {
   const unitHex = (+token.unit).toString(16).padStart(2, '0');
   const symbolLengthHex = token.symbol.length.toString(16).padStart(2, '0');
   const symbolHex = Buffer.from(token.symbol).toString('hex').padEnd(14, '0');
@@ -14,3 +15,10 @@ console.log('OKX USDT: ', buildTokenHex(OKX.tokens.USDT));
 console.log('OKX USDC: ', buildTokenHex(OKX.tokens.USDC));
 console.log('ZKS USDC: ', buildTokenHex(ZKSYNC.tokens.USDC));
 console.log('ZKS WETH: ', buildTokenHex(ZKSYNC.tokens.WETH));
+
+const tokenGALA: BuildTokenHexInput = {
+  symbol: 'GALA',
+  unit: '8',
+  contractAddress: '0xd1d2Eb1B1e90B638588728b4130137D262C87cae',
+};
+console.log('ETH GALA: ', buildTokenHex(tokenGALA));
