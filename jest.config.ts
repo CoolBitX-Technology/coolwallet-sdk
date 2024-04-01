4fbc45a6b5e0c2828d5edea38fd27e80edbc6fb1
Tom Lin <gundomlegend@gmail.com>
2024-04-01 16:59:50 +0800

    fix: 忽略其他鏈測試檔案

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,11 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types';
 
 // Sync object
 const config: Config.InitialOptions = {
   verbose: false,
   automock: false,
+  // ingore broken coin-tests
+  testPathIgnorePatterns: ['/packages/coin-trx/', '/packages/coin-atom/', 'packages/coin-theta'],
+  testMatch: ['**/*.test.(js|ts)?(x)'],
 };
 export default config;
