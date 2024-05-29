233b0de0ea07e8cbb36f4cb836e741b1d816ac64
Brian Yeh <brian.yeh@coolbitx.com>
2024-05-29 16:05:20 +0800

    fix(ton): extends EDDSACoin to support getPublicKey

--- packages/coin-ton/src/index.ts
+++ packages/coin-ton/src/index.ts
@@ -1,7 +1,13 @@
 import { coin as COIN, utils, config, Transport } from '@coolwallet/core';
 import * as AddressUtils from './utils/addressUtils';
+import { COIN_TYPE } from './config/param';
 
-export default class TON implements COIN.Coin {
+
+export default class TON extends COIN.EDDSACoin implements COIN.Coin {
+  constructor() {
+    super(COIN_TYPE);
+  }
+
   async getAddress(transport: Transport, appPrivateKey: string, appId: string, addressIndex: number): Promise<string> {
     const fullPath = utils.getFullPath({ pathString: `44'/607'/${addressIndex}'`, pathType: config.PathType.SLIP0010 });
     const publicKey = await COIN.getPublicKeyByPath(transport, appId, appPrivateKey, fullPath); // need to connect card
