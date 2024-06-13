6842f8b596630f559cde2ec38ed73ffd854aa900
dorac <dorac@coolbitx.com>
2024-06-14 02:08:51 +0800

    refactor: update dependencies and adjust codebase

--- packages/transport-nfc/src/index.ts
+++ packages/transport-nfc/src/index.ts
@@ -1,16 +1,1 @@
-import NfcTransport from './transport';
-import type { Transport } from '@coolwallet/core';
-import type { Device } from 'react-native-ble-plx';
-
-const createTransport = (): Promise<Transport | undefined> =>
-  new Promise((resolve, reject) => {
-    const transport = new NfcTransport();
-    transport
-      .request('000980CA00000000000000', '')
-      .then(() => {
-        resolve(transport);
-      })
-      .catch(reject);
-  });
-
-export { createTransport, NfcTransport };
+export { default as NfcTransport } from './transport';
