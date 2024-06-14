07baa5aecbffa7a5d06a62b32efdf8ee76c72bf8
Hank Liu <hankliu@coolbitx.com>
2024-06-14 18:51:11 +0800

    update  transport contructor to card type selectable

--- packages/transport-jre-http/src/index.ts
+++ packages/transport-jre-http/src/index.ts
@@ -1,9 +1,9 @@
 import HttpTransport from './transport';
-import type { Transport } from '@coolwallet/core';
+import { CardType, Transport } from '@coolwallet/core';
 
-const createTransport = (baseURL = 'http://localhost:9527'): Promise<Transport | undefined> =>
+const createTransport = (baseURL = 'http://localhost:9527', cardType?: CardType): Promise<Transport | undefined> =>
   new Promise((resolve, reject) => {
-    const transport = new HttpTransport(baseURL);
+    const transport = new HttpTransport(baseURL, cardType);
     transport
       .request('000980CA00000000000000', '')
       .then(() => {
@@ -13,4 +13,3 @@ const createTransport = (baseURL = 'http://localhost:9527'): Promise<Transport |
   });
 
 export { createTransport, HttpTransport };
-
