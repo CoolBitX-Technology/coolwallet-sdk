07e440a1a533725affaf3b65adea75c250a2e021
Hank Liu <hankliu@coolbitx.com>
2024-07-26 17:57:14 +0800

    add import and export command

--- packages/core/src/setting/backup.ts
+++ packages/core/src/setting/backup.ts
@@ -1,3 +1,4 @@
+import { auth } from '.';
 import { commands } from '../apdu/execute/command';
 import { executeCommand } from '../apdu/execute/execute';
 import { target } from '../config/param';
@@ -61,7 +62,7 @@ export const checkBackupStatus = async (transport: Transport): Promise<boolean> 
  * @param {string} signedCommand
  * @return {Promise<{ status: boolean, statusCode: string, msg: string }>}
  */
-export const deleteSeedBackup = async (transport: Transport, signature: string) => {
+export const deleteSeedBackup = async (transport: Transport, signature: string): Promise<boolean> => {
   const { statusCode, msg } = await executeCommand(
     transport,
     commands.DELETE_REGISTER_BACKUP,
@@ -74,5 +75,63 @@ export const deleteSeedBackup = async (transport: Transport, signature: string) 
     return true;
   } else {
     throw new APDUError(commands.DELETE_REGISTER_BACKUP, statusCode, msg);
+  }
+};
+
+/**
+ * Export backup data.
+ * @param transport
+ * @param appId
+ * @param appPrivateKey
+ * @param backupCardId
+ * @returns
+ */
+export const exportBackupData = async (
+  transport: Transport,
+  appId: string,
+  appPrivateKey: string,
+  backupCardId: string
+): Promise<string> => {
+  const signature = await auth.getCommandSignature(
+    transport,
+    appId,
+    appPrivateKey,
+    commands.DELETE_REGISTER_BACKUP,
+    backupCardId
+  );
+  const { statusCode, msg } = await executeCommand(
+    transport,
+    commands.DELETE_REGISTER_BACKUP,
+    target.SE,
+    backupCardId + signature,
+    undefined,
+    undefined
+  );
+  if (statusCode === CODE._9000) {
+    return msg;
+  } else {
+    throw new APDUError(commands.EXPORT_BACKUP_DATA, statusCode, msg);
+  }
+};
+
+/**
+ * Import backup data.
+ * @param transport
+ * @param backupData
+ * @returns
+ */
+export const importBackupData = async (transport: Transport, backupData: string): Promise<string> => {
+  const { statusCode, msg } = await executeCommand(
+    transport,
+    commands.IMPORT_BACKUP_DATA,
+    target.SE,
+    backupData,
+    undefined,
+    undefined
+  );
+  if (statusCode === CODE._9000) {
+    return msg;
+  } else {
+    throw new APDUError(commands.IMPORT_BACKUP_DATA, statusCode, msg);
   }
 };
