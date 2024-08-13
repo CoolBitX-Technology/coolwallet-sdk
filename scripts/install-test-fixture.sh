5e67f9b93fee6b06c9539e845cfac3c1550f1a7f
Brian Yeh <brian.yeh@coolbitx.com>
2024-08-13 10:07:22 +0800

    chore: add script to install test fixture

---
+++ scripts/install-test-fixture.sh
@@ -0,0 +1,5 @@
+TEST_FIXTURE_SCOPES="--scope @coolwallet/core --scope @coolwallet/testing-library --scope @coolwallet/transport-jre-http"
+LERNA="npx lerna"
+
+$LERNA bootstrap $TEST_FIXTURE_SCOPES
+$LERNA run build $TEST_FIXTURE_SCOPES
