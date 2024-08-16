4a7d78eaf180a7aac2dda914b781a3a2f6efd34a
Tom Lin <gundomlegend@gmail.com>
2024-08-16 18:23:23 +0800

    refactor: 調整寫法

--- packages/coin-kas/src/index.ts
+++ packages/coin-kas/src/index.ts
@@ -1,13 +1,16 @@
 
 import { coin as COIN, Transport } from '@coolwallet/core';
 import { COIN_TYPE } from './config/param';
-import { getAddressByPublicKey } from './utils/address';
-import { SignTransferTxType } from './config/type';
+import { getAddressByPublicKey, addressToOutScript, pubkeyToPayment } from './utils/address';
 import signTransferTransaction from './sign';
+import { Script, ScriptType, SignTxType } from './config/type';
 
 export default class KAS extends COIN.ECDSACoin implements COIN.Coin {
+  public addressToOutScript: (address: string) => Script;
+  
   constructor() {
     super(COIN_TYPE);
+    this.addressToOutScript = addressToOutScript;
   }
 
   async getAddress(
@@ -21,10 +24,50 @@ export default class KAS extends COIN.ECDSACoin implements COIN.Coin {
     return getAddressByPublicKey(publicKey);
   }
 
-  async signTransaction(signTxType: SignTransferTxType): Promise<string> {
-    const { transport, appPrivateKey, appId, addressIndex } = signTxType;
-    const changePublicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex);
-    const changeAddress = getAddressByPublicKey(changePublicKey);
-    return signTransferTransaction(signTxType, changeAddress);
+  async getAddressAndOutScript(
+    transport: Transport,
+    appPrivateKey: string,
+    appId: string,
+    scriptType: ScriptType,
+    addressIndex: number,
+    purpose?: number
+  ): Promise<{ address: string; outScript: Buffer }> {
+    const publicKey = await this.getPublicKey(transport, appPrivateKey, appId, addressIndex, purpose);
+    return pubkeyToPayment(publicKey, scriptType);
+  }
+
+  async getAddressAndOutScriptByAccountKey(
+    accPublicKey: string,
+    accChainCode: string,
+    addressIndex: number,
+    scriptType: ScriptType
+  ): Promise<{ address: string; outScript: Buffer }> {
+    const publicKey = this.getAddressPublicKey(accPublicKey, accChainCode, addressIndex);
+    return pubkeyToPayment(publicKey, scriptType);
+  }
+
+  async signTransaction(signTxType: SignTxType): Promise<string> {
+    const { inputs, transport, appPrivateKey, appId, change } = signTxType;
+    for (const input of inputs) {
+      const pubkey = await this.getPublicKey(
+        transport,
+        appPrivateKey,
+        appId,
+        input.addressIndex,
+        input.purposeIndex
+      );
+      input.pubkeyBuf = Buffer.from(pubkey, 'hex');
+    }
+    if (change) {
+      const pubkey = await this.getPublicKey(
+        transport,
+        appPrivateKey,
+        appId,
+        change.addressIndex,
+        change.purposeIndex
+      );
+      change.pubkeyBuf = Buffer.from(pubkey, 'hex');
+    }
+    return signTransferTransaction(signTxType);
   }
 }
