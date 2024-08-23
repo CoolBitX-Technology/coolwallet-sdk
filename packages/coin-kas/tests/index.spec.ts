d270cb9f14d3884f6b347d69ea11204201a3b601
Tom Lin <gundomlegend@gmail.com>
2024-08-23 16:34:23 +0800

    feat: fix test & add sign tx test

--- packages/coin-kas/tests/index.spec.ts
+++ packages/coin-kas/tests/index.spec.ts
@@ -2,7 +2,7 @@ import { Transport } from '@coolwallet/core';
 import { createTransport } from '@coolwallet/transport-jre-http';
 import { initialize } from '@coolwallet/testing-library';
 import KAS from '../src';
-import { ScriptType } from '../src/config/types';
+import { ScriptType, SignTxType } from '../src/config/types';
 
 type PromiseValue<T> = T extends Promise<infer V> ? V : never;
 type Mandatory = PromiseValue<ReturnType<typeof initialize>>;
@@ -28,5 +28,34 @@ describe('Test KAS SDK', () => {
     const addressIndex = 0;
     const address = await kasSDK.getAddress(transport, props.appPrivateKey, props.appId, ScriptType.P2PK, addressIndex);
     expect(address).toEqual(testWalletInfo.address);
+  });
+
+  it.skip('Test Sign Transaction', async () => {
+    const signTxType: SignTxType = {
+      transport,
+      appPrivateKey: props.appPrivateKey,
+      appId: props.appId,
+      scriptType: ScriptType.P2PK,
+      inputs: [
+        {
+          preIndex: 1,
+          preTxHash: '21aa1aff85fc054381f8536d1ab8dbe68f1673a7c67b7ad7816431603c58d32e',
+          preValue: "2355557326",
+          purposeIndex: 44,
+          addressIndex: 0,
+        }
+      ],
+      output: {
+        value: "1000",
+        address: "kaspa:qq9rmfhgc758j4zquc8yvcngd0qhekt90qqf2czvshvdzdlq7jq8jphmvr028",
+      },
+      change: {
+        value: "2355546146",
+        addressIndex: 0,
+        purposeIndex: 44,
+      }
+    };
+    const signedTx = await kasSDK.signTransaction(signTxType);
+    console.log('>>> signedTx=',signedTx);
   });
 });
