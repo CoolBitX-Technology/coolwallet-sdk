4eea2f5019c2d2a95bf67520b66e743b052d4eae
jin860315 <jin860315@gmail.com>
2024-09-25 18:34:53 +0800

    chore: add script to run all test case

---
+++ scripts/run-all-test.sh
@@ -0,0 +1,20 @@
+TEST_FIXTURE_SCOPES="--scope @coolwallet/core --scope @coolwallet/testing-library --scope @coolwallet/transport-jre-http"
+TEST_SCOPESS=(
+  --scope @coolwallet/btc
+  --scope @coolwallet/dot
+  --scope @coolwallet/kas
+  --scope @coolwallet/ton
+  --scope @coolwallet/bsc
+  --scope @coolwallet/evm
+  --scope @coolwallet/eth
+  --scope @coolwallet/sol
+  --scope @coolwallet/ada
+  --scope @coolwallet/terra
+  --scope @coolwallet/cosmos
+  --scope @coolwallet/xtz
+)
+LERNA="npx lerna"
+
+$LERNA bootstrap $TEST_FIXTURE_SCOPES
+$LERNA run build $TEST_FIXTURE_SCOPES
+$LERNA run test "${TEST_SCOPES[@]}" --concurrency 1
