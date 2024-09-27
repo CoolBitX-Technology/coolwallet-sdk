d27c45e25b1358fb1428ad9859784aaab5592efa
Brian Yeh <brian.yeh@coolbitx.com>
2024-09-27 10:51:01 +0800

    style: format files

--- scripts/run-all-test-report.sh
+++ scripts/run-all-test-report.sh
@@ -34,14 +34,14 @@ mkdir -p scripts/reports/junit
 for dir in packages/coin-* packages/core; do
   if [ -d "$dir" ] && [ -f "$dir/junit.xml" ]; then
     coin_name=$(basename "$dir")
-    sed 's/&quot;/"/g' "$dir/junit.xml" > "scripts/reports/junit/${coin_name}-junit.xml" || echo "Failed to process $dir/junit.xml"
+    sed 's/&quot;/"/g' "$dir/junit.xml" >"scripts/reports/junit/${coin_name}-junit.xml" || echo "Failed to process $dir/junit.xml"
   fi
 done
 
 # 移除所有 junit.xml 文件
 find packages -type f -name 'junit.xml' -exec rm -f {} +
 
-# 產生 Allure report 
+# 產生 Allure report
 npx allure generate ./scripts/reports/junit --clean -o ./scripts/allure-report
 
 # open Allure report
