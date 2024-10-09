f03558a7e8470bf602708a33a6442c72a26694e9
jin860315 <jin860315@gmail.com>
2024-10-09 10:49:17 +0800

    fix: auto test error

--- scripts/handing-report.sh
+++ scripts/handing-report.sh
@@ -1,5 +1,11 @@
+echo "Starting to handle report"
+
+ls -l /packages/coin-ada
+
 # 建立測試報告目錄
 mkdir -p scripts/reports/junit
+
+echo "mkdir file finished"
 
 # 移動測試報告
 for dir in packages/coin-* packages/core; do
@@ -9,5 +15,11 @@ for dir in packages/coin-* packages/core; do
   fi
 done
 
+echo "move junit file finished"
+
+ls -l /scripts/reports/junit
+
 # 移除所有 junit.xml 文件
 find packages -type f -name 'junit.xml' -exec rm -f {} +
+
+echo "remove junit file finished"
