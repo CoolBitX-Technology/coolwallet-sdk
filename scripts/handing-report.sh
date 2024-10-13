97197e0e05e93c5b95d9cb000a55f57d9c3e6ddf
jin860315 <jin860315@gmail.com>
2024-10-13 20:56:09 +0800

    fix: show error trace on report

--- scripts/handing-report.sh
+++ scripts/handing-report.sh
@@ -5,7 +5,7 @@ mkdir -p scripts/reports/junit
 for dir in packages/coin-* packages/core; do
   if [ -d "$dir" ] && [ -f "$dir/junit.xml" ]; then
     coin_name=$(basename "$dir")
-    sed 's/&quot;/"/g' "$dir/junit.xml" >"scripts/reports/junit/${coin_name}-junit.xml" || echo "Failed to process $dir/junit.xml"
+    sed 's|<failure>|<failure message="Error">|g' "$dir/junit.xml" >"scripts/reports/junit/${coin_name}-junit.xml" || echo "Failed to process $dir/junit.xml"
   fi
 done
 
