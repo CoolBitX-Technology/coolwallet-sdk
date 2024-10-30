67413849b84f053715dddc406a49b4ea46d1e102
jin860315 <jin860315@gmail.com>
2024-10-30 20:21:29 +0800

    chore: add npm version patch

--- scripts/update-core-version.sh
+++ scripts/update-core-version.sh
@@ -1,43 +1,66 @@
 #!/bin/sh
 
-# sh scripts/update-core-version.sh     
+# sh scripts/update-core-version.sh    
+ 
+IGNORE_SCOPES=(
+   --ignore @coolwallet/core
+   --ignore @coolwallet/transport-react-native-ble
+   --ignore tester
+   --ignore @coolwallet/testing-library
+   --ignore @coolwallet/template
+)
 
 # 將 npm info 輸出到 JSON 文件
-npm info @coolwallet/core --json > package-info.json
+npx lerna list --json > package-info.json
 
-time_data=$(grep -o '"1\.[0-9]\+\.[0-9]\+[^"]*": "[^"]*"' package-info.json)
+# 目前 core 最新版本
+core_latest_version=$(grep '"@coolwallet/core"' package-info.json -A 3 | grep '"version"' | sed 's/.*: "\(.*\)".*/\1/')
+core_latest_version="^$core_latest_version"
+echo "core_latest_version: $core_latest_version" 
 
-# 確認 time_data 是否有內容
-if [ -z "$time_data" ]; then
-    echo "No versions found in time_data."
-    exit 1
+# 讀取 package-info.json 的內容
+json_content=$(cat package-info.json)
+
+# 拿到各個幣種 name 和 version
+names=$(echo "$json_content" | grep -o '"name": *"[^"]*"' | awk -F'"' '{print $4}')
+versions=$(echo "$json_content" | grep -o '"version": *"[^"]*"' | awk -F'"' '{print $4}')
+
+# 將 name 和 version 轉換為陣列
+names_array=($names)
+versions_array=($versions)
+
+# 確保兩個陣列的長度相同
+if [ ${#names_array[@]} -ne ${#versions_array[@]} ]; then
+  echo "Error: The number of names and versions do not match."
+  exit 1
 fi
 
-echo "Time data:"
-echo "$time_data"
+# 迭代每個 name 和 version，執行 npm show 這邊主要要判斷是否需要更新各幣種版本，如果不需要會忽略這個 package
+for i in "${!names_array[@]}"; do
+  name=${names_array[$i]}
+  version=${versions_array[$i]}
+  peer_dependencies=$(npm show $name@$version peerDependencies --json)
 
-# 提取所有版本和時間，並按時間排序，取出最新的版本
-latest_version=$(echo "$time_data" | grep -o '"1\.[0-9]\+\.[0-9][^"]*": "[^"]*"' | sort -t '"' -k 4,4r | head -n 1)
-echo "latest_version: $latest_version" 
+  # 提取 @coolwallet/core 的版本
+  core_version=$(echo "$peer_dependencies" | grep -o '"@coolwallet/core": "[^"]*"' | sed 's/.*: "//; s/"$//')
 
-# 輸出最新版本
-if [ -n "$latest_version" ]; then
-    final_version=$(echo "$latest_version" | sed 's/":.*//;s/"//g')
-    echo "Latest version: $final_version"
-else
-    echo "No version found."
-fi
+  if [ -z "$core_version" ]; then
+   echo "No peer dependencies found for $name@$version."
+  else
+   echo "Peer Dependencies for $name@$version:"
+   echo "$core_version"
 
-# 組出要執行的 command
-command="npm install @coolwallet/core@$final_version --save-peer"
-echo "command: $command"
+   if [ "$core_latest_version" = "$core_version" ]; then
+     IGNORE_SCOPES+=("--ignore" "$name")
+    fi
+  fi
+done
 
-IGNORE_SCOPES=(
-  --ignore @coolwallet/core
-  --ignore @coolwallet/transport-react-native-ble
-  --ignore tester
-  --ignore @coolwallet/testing-library
-  --ignore @coolwallet/template
-)
-# 執行 lerna command
-npx lerna exec "${IGNORE_SCOPES[@]}" -- "$command"
+# # 組出要執行的 command
+ command="npm install @coolwallet/core@$core_latest_version --save-peer"
+ echo "command: $command"
+
+# # 執行 lerna command
+echo "IGNORE_SCOPES: ${IGNORE_SCOPES[@]}"
+npx lerna exec  "${IGNORE_SCOPES[@]}" -- "$command" && \
+npx lerna exec  "${IGNORE_SCOPES[@]}" -- npm version patch
