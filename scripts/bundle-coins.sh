0099445ee0cf6421b89874923499c68eb36af2cb
Brian Yeh <brian.yeh@coolbitx.com>
2024-11-05 19:27:13 +0800

    chore: add bundle-coins.sh

---
+++ scripts/bundle-coins.sh
@@ -0,0 +1,13 @@
+TEST_FIXTURE_SCOPES=(
+  --scope @coolwallet/eth
+  --scope @coolwallet/evm
+  --scope @coolwallet/sol
+)
+
+LERNA="npx lerna"
+
+$LERNA bootstrap "${TEST_FIXTURE_SCOPES[@]}"
+$LERNA run build "${TEST_FIXTURE_SCOPES[@]}"
+
+# Delete node_modules for specific packages
+$LERNA exec "${TEST_FIXTURE_SCOPES[@]}" -- rm -rf node_modules
