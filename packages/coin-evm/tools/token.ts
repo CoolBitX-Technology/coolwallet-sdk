1e65b9b8b592b18849aa6b10c9201e1a2779d58b
Brian Yeh <brian.yeh@coolbitx.com>
2024-11-20 15:20:06 +0800

    chore(eth): add script to generate token hex

--- packages/coin-evm/tools/token.ts
+++ packages/coin-evm/tools/token.ts
@@ -20,31 +20,6 @@ console.log('BASE USDbC: ', buildTokenHex(BASE.tokens.USDbC));
 console.log('BASE axlUSDC: ', buildTokenHex(BASE.tokens.axlUSDC));
 console.log('BASE BSWAP: ', buildTokenHex(BASE.tokens.BSWAP));
 
-console.log(
-  'ETH GALA: ',
-  buildTokenHex({
-    symbol: 'GALA',
-    unit: '8',
-    contractAddress: '0xd1d2Eb1B1e90B638588728b4130137D262C87cae',
-  })
-);
-console.log(
-  'ETH EURC: ',
-  buildTokenHex({
-    symbol: 'EURC',
-    unit: '6',
-    contractAddress: '0x1abaea1f7c830bd89acc67ec4af516284b1bc33c',
-  })
-);
-console.log(
-  'ETH WCT: ',
-  buildTokenHex({
-    symbol: 'WCT',
-    unit: '18',
-    contractAddress: '0x61cc6aF18C351351148815c5F4813A16DEe7A7E4',
-  })
-);
-
 console.log('OP USDC.e: ', buildTokenHex(OPTIMISM.tokens['USDC.e']));
 console.log('OP USDC: ', buildTokenHex(OPTIMISM.tokens.USDC));
 console.log('OP WCT: ', buildTokenHex(OPTIMISM.tokens.WCT));
