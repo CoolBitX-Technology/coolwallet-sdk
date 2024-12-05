1f7d810cc5500cb448bc9fc30c8200e9cb11118c
Tom Lin <gundomlegend@gmail.com>
2024-12-05 10:13:27 +0800

    feat: 改用 @mysten/sui library

--- packages/coin-sui/src/config/types.ts
+++ packages/coin-sui/src/config/types.ts
@@ -1,7 +1,20 @@
+import { Transport } from "@coolwallet/core";
+import { Transaction } from '@mysten/sui/transactions';
+
 export type TOKENTYPE = {
   name: string;
   symbol: string;
   decimals: number;
   address: string;
-  signature: string;
+};
+
+export type TransactionArgs = {
+  transport: Transport;
+  appPrivateKey: string;
+  appId: string;
+  transaction: Transaction;
+  addressIndex: number;
+  tokenInfo?: TOKENTYPE;
+  confirmCB?(): void;
+  authorizedCB?(): void;
 };
--- packages/coin-sui/src/index.ts
+++ packages/coin-sui/src/index.ts
@@ -1,28 +1,28 @@
 import { coin as COIN, Transport, utils } from '@coolwallet/core';
-import { COIN_TYPE } from './config/param';
+import { COIN_TYPE, SCRIPT } from './config/param';
 import { PathType } from '@coolwallet/core/lib/config';
-import { fromBase64 } from './bcs/b64';
-import { Ed25519PublicKey } from './cryptography/ed25519-publickey';
+import { Ed25519PublicKey } from '@mysten/sui/dist/cjs/keypairs/ed25519/publickey';
+import { TransactionArgs } from './config/types';
+import { signAllTransaction } from './sign';
+export { TOKENTYPE } from './config/types';
+export { Ed25519PublicKey } from '@mysten/sui/keypairs/ed25519';
 
 export default class Sui extends COIN.EDDSACoin implements COIN.Coin {
   constructor() {
     super(COIN_TYPE);
   }
 
-  getAddressFromPublic(publicKey: string) {
-    const pk = fromBase64(publicKey);
-    const p = new Ed25519PublicKey(pk);
-    return p.toSuiAddress();
-  }
-
   async getAddress(transport: Transport, appPrivateKey: string, appId: string, addressIndex: number): Promise<string> {
     const path = utils.getFullPath({ pathType: PathType.SLIP0010, pathString: `44'/784'/0'/0'/${addressIndex}'` });
     const publicKey = await COIN.getPublicKeyByPath(transport, appId, appPrivateKey, path);
     const base64 = Buffer.from(publicKey, 'hex').toString('base64');
-    return this.getAddressFromPublic(base64);
+    return new Ed25519PublicKey(base64).toSuiAddress();
   }
 
-  async signTransaction(): Promise<string> {
-    throw new Error('Not implemented');
+  async signTransaction(transactionArgs: TransactionArgs): Promise<string> {
+    const { tokenInfo } = transactionArgs;
+    const script = !tokenInfo ? SCRIPT.TRANSFER.scriptWithSignature : SCRIPT.TOKEN_TRANSFER.scriptWithSignature;
+    const argument = !tokenInfo ? SCRIPT.TRANSFER.argument : SCRIPT.TOKEN_TRANSFER.argument;
+    signAllTransaction(transactionArgs, script, argument);
   }
 }
