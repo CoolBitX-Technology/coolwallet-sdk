8699f5d1092b94a0eab46d3e7a43e97fd2134951
Melody Lee <melody0000tw@gmail.com>
2024-12-09 17:28:27 +0800

    feat: pass intentMessage hex to instead of tx hex in getCoinTransferArguments

--- packages/coin-sui/src/utils/scriptUtil.ts
+++ packages/coin-sui/src/utils/scriptUtil.ts
@@ -1,7 +1,7 @@
 import { PathType } from "@coolwallet/core/lib/config";
 import { utils } from "@coolwallet/core";
 import { Transaction } from "@mysten/sui/transactions";
-
+import { messageWithIntent } from '@mysten/sui/dist/cjs/cryptography/intent';
 
 /**
  * getTransferArguments
@@ -17,42 +17,43 @@ function getTargetIndex(source: string, target: string) {
   return index;
 }
 
-function getToAddressHexIndex(rawTx: Transaction, txHex: string): string {
+function getToAddressHexIndex(rawTx: Transaction, hex: string): string {
   const transferObjects = rawTx.getData().commands.filter((command) => command.TransferObjects);
   const toAddressIndex = transferObjects[0].TransferObjects?.address.Input;
   const toAddressBase64 = rawTx.getData().inputs[toAddressIndex].Pure?.bytes;
   if (!toAddressBase64) throw new Error(`getCoinTransferArguments.getToAddressHexIndex >>> toAddressBase64 not found`);
   const toAddress = Buffer.from(toAddressBase64, 'base64').toString('hex');
-  const toAddressHexIndex = getTargetIndex(txHex, toAddress).toString(16).padStart(2, '0');
+  const toAddressHexIndex = getTargetIndex(hex, toAddress).toString(16).padStart(3, '0');
   return toAddressHexIndex;
 }
 
-function getSendAmountHexIndex(rawTx: Transaction, txHex: string): string {
+function getSendAmountHexIndex(rawTx: Transaction, hex: string): string {
   const splitCoins = rawTx.getData().commands.filter((command) => command.SplitCoins);
   const sendAmountIndex = splitCoins[0].SplitCoins?.amounts[0].Input;
   const sendAmountBase64 = rawTx.getData().inputs[sendAmountIndex].Pure?.bytes;
   if (!sendAmountBase64)
     throw new Error(`getCoinTransferArguments.getSendAmountHexIndex >>> sendAmountBase64 not found`);
   const sendAmountLittleEndian = Buffer.from(sendAmountBase64, 'base64').toString('hex');
-  const amountHexIndex = getTargetIndex(txHex, sendAmountLittleEndian).toString(16).padStart(2, '0');
+  const amountHexIndex = getTargetIndex(hex, sendAmountLittleEndian).toString(16).padStart(3, '0');
   return amountHexIndex;
 }
 
 async function getCoinTransferArguments(rawTx: Transaction, addressIndex: number): Promise<string> {
   const path = utils.getFullPath({ pathType: PathType.SLIP0010, pathString: `44'/784'/0'/0'/${addressIndex}'` });
   const SEPath = `11${path}`;
-  console.debug('SEPath: ', SEPath);
+  console.debug('SUI.getCoinTransferArguments >>> SEPath: ', SEPath);
 
   const txBytes = await rawTx.build();
-  const txHex = Buffer.from(txBytes).toString('hex');
-  console.debug('txHex: ', txHex);
+  const intentMessage = messageWithIntent('TransactionData', txBytes); // tx.sign 需要先包一層 intentMessage 才能簽署
+  const intentMessageHex = Buffer.from(intentMessage).toString('hex');
+  console.debug('SUI.getCoinTransferArguments >>> intentMessage: ', intentMessageHex);
 
-  const toAddressHexIndex = getToAddressHexIndex(rawTx, txHex);
-  const amountHexIndex = getSendAmountHexIndex(rawTx, txHex);
+  const toAddressHexIndex = getToAddressHexIndex(rawTx, intentMessageHex);
+  const amountHexIndex = getSendAmountHexIndex(rawTx, intentMessageHex);
   const header = toAddressHexIndex + amountHexIndex;
-  console.debug('header: ', header);
+  console.debug('SUI.getCoinTransferArguments >>> header: ', header);
 
-  return SEPath + header + txHex;
+  return SEPath + header + intentMessageHex;
 }
 
 export { getCoinTransferArguments };
