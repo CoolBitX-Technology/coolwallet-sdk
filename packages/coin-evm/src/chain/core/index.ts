8044570a28d05bcaec3f72b364de20fca43c68c5
Brian Yeh <brian.yeh@coolbitx.com>
2025-02-03 13:28:29 +0800

    feat: add CORE chain signature

--- packages/coin-evm/src/chain/core/index.ts
+++ packages/coin-evm/src/chain/core/index.ts
@@ -4,8 +4,8 @@ import { ChainProps } from '../types';
 class CoreChain extends ChainProps {
   id = 1116;
   symbol = 'CORE';
-  signature = // TODO: Core Chain
-    ``.padStart(
+  signature =
+    `304502205110D665BD7296087088D030598FDA9A6A09F3A5FB41CCB270B1BF43A478A9D5022100D35CE88FA465F36517DB9D072291CDA10600EFCFF05260A64545078BCD6CBD23`.padStart(
       144,
       '0'
     );
