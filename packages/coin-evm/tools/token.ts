15c6666050976f17f14db292b8280a8f4e8a0337
Tom Lin <gundomlegend@gmail.com>
2025-02-24 10:49:14 +0800

    chore: update arbitrum USDT -> USD₮0

--- packages/coin-evm/tools/token.ts
+++ packages/coin-evm/tools/token.ts
@@ -12,6 +12,7 @@ const buildTokenHex = (token: BuildTokenHexInput) => {
 };
 
 console.log('ARB ORDER: ', buildTokenHex(ARBITRUM.tokens.ORDER));
+console.log('ARB USD₮0: ', buildTokenHex(ARBITRUM.tokens['USD₮0']));
 console.log('OKX USDT: ', buildTokenHex(OKX.tokens.USDT));
 console.log('OKX USDC: ', buildTokenHex(OKX.tokens.USDC));
 console.log('ZKS USDC: ', buildTokenHex(ZKSYNC.tokens.USDC));
@@ -32,9 +33,5 @@ console.log('AVAXC EURC: ', buildTokenHex(AVAXC.tokens.EURC));
 console.log('AVAXC ORDER: ', buildTokenHex(AVAXC.tokens.ORDER));
 
 console.log('CORE stCORE: ', buildTokenHex(CORE.tokens.stCORE));
-console.log('CORE WBNB: ', buildTokenHex(CORE.tokens.WBNB));
-console.log('CORE BTCB: ', buildTokenHex(CORE.tokens.BTCB));
-console.log('CORE WBTC: ', buildTokenHex(CORE.tokens.WBTC));
 console.log('CORE USDT: ', buildTokenHex(CORE.tokens.USDT));
 console.log('CORE USDC: ', buildTokenHex(CORE.tokens.USDC));
-console.log('CORE WETH: ', buildTokenHex(CORE.tokens.WETH));
