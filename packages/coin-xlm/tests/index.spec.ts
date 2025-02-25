f7636a592b4a0cc9aad9cdcb4ccb2b59d1f5b3a4
Melody Lee <melody0000tw@gmail.com>
2025-02-25 10:23:09 +0700

    chore: add KAU tests

--- packages/coin-xlm/tests/index.spec.ts
+++ packages/coin-xlm/tests/index.spec.ts
@@ -227,7 +227,7 @@ describe('Test XLM SDK', () => {
     });
   });
 
-  describe.only('Test KAG CoinType', () => {
+  describe('Test KAG CoinType', () => {
     let transport: Transport;
     let props: Mandatory;
     let cardType: CardType;
@@ -439,6 +439,225 @@ describe('Test XLM SDK', () => {
             163,
             122,
             7,
+          ],
+          "type": "Buffer",
+        }
+      `);
+    });
+  });
+
+  describe.only('Test KAU CoinType', () => {
+    let transport: Transport;
+    let props: Mandatory;
+    let cardType: CardType;
+    const xlmSDK = new XLM(types.COIN_SPECIES.KAU);
+    const mnemonic = 'zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo abstract';
+
+    beforeAll(async () => {
+      if (process.env.CARD === 'lite') {
+        cardType = CardType.Lite;
+      } else {
+        cardType = CardType.Pro;
+      }
+
+      if (cardType === CardType.Lite) {
+        transport = (await createTransport('http://localhost:9527', CardType.Lite))!;
+      } else {
+        transport = (await createTransport())!;
+      }
+      props = await initialize(transport, mnemonic);
+    });
+
+    it('Test Get index 0 address', async () => {
+      const address = await xlmSDK.getAddress(transport, props.appPrivateKey, props.appId, types.PROTOCOL.BIP44);
+      expect(address).toMatchInlineSnapshot(`"GAMHFD24LF6LASLUPBFTVTDNFBZAPFOWSUXDQ6F5MW6S6UCDCBY2JAWY"`);
+    });
+
+    it('Test Get index 1 address', async () => {
+      const address = await xlmSDK.getAddress(transport, props.appPrivateKey, props.appId, types.PROTOCOL.SLIP0010);
+      expect(address).toMatchInlineSnapshot(`"GD6Q5TIZH6JUZ4MM5ULAB6GWRADEALONNSGMZRIGSDAHYXNJ2INWOP3F"`);
+    });
+
+    it('Test Sign Transfer KAG Transaction for protocol SLIP0010', async () => {
+      const transaction: types.signTxType = {
+        transport: transport,
+        appPrivateKey: props.appPrivateKey,
+        appId: props.appId,
+        transaction: {
+          from: '3a30655dcbf097ff5ccc3f6bb6a809cda73589c2b9d826551d3136717a0a993a',
+          to: '2b78433e9bc17233ee14d822e10ff0d8ad61f37c990e9318275defd127e3bcda',
+          amount: '10000000',
+          fee: '5000000',
+          sequence: '215752966240469006',
+          minTime: 0,
+          maxTime: 0,
+          memoType: 'none',
+          memo: '',
+          isCreate: true,
+        },
+        protocol: types.PROTOCOL.SLIP0010,
+      };
+      const signedTx = await xlmSDK.signTransaction(transaction);
+      expect(signedTx).toMatchInlineSnapshot(`
+        Object {
+          "data": Array [
+            87,
+            235,
+            68,
+            107,
+            116,
+            56,
+            216,
+            196,
+            124,
+            145,
+            22,
+            112,
+            116,
+            224,
+            10,
+            53,
+            104,
+            118,
+            113,
+            209,
+            56,
+            75,
+            161,
+            69,
+            154,
+            212,
+            110,
+            66,
+            13,
+            24,
+            65,
+            202,
+            128,
+            133,
+            41,
+            188,
+            43,
+            233,
+            106,
+            102,
+            234,
+            106,
+            246,
+            177,
+            73,
+            206,
+            216,
+            64,
+            97,
+            176,
+            0,
+            61,
+            98,
+            14,
+            43,
+            64,
+            42,
+            255,
+            228,
+            70,
+            217,
+            225,
+            186,
+            15,
+          ],
+          "type": "Buffer",
+        }
+      `);
+    });
+
+    it('Test Sign Transfer KAG Transaction for protocol BIP44', async () => {
+      const transaction: types.signTxType = {
+        transport: transport,
+        appPrivateKey: props.appPrivateKey,
+        appId: props.appId,
+        transaction: {
+          from: '3a30655dcbf097ff5ccc3f6bb6a809cda73589c2b9d826551d3136717a0a993a',
+          to: '2b78433e9bc17233ee14d822e10ff0d8ad61f37c990e9318275defd127e3bcda',
+          amount: '10000000',
+          fee: '5000000',
+          sequence: '215752966240469006',
+          minTime: 0,
+          maxTime: 0,
+          memoType: 'none',
+          memo: '',
+          isCreate: true,
+        },
+        protocol: types.PROTOCOL.BIP44,
+      };
+      const signedTx = await xlmSDK.signTransaction(transaction);
+      expect(signedTx).toMatchInlineSnapshot(`
+        Object {
+          "data": Array [
+            78,
+            0,
+            41,
+            46,
+            153,
+            72,
+            156,
+            239,
+            138,
+            25,
+            159,
+            4,
+            220,
+            31,
+            64,
+            168,
+            156,
+            244,
+            68,
+            180,
+            38,
+            191,
+            206,
+            188,
+            201,
+            101,
+            14,
+            134,
+            147,
+            78,
+            87,
+            99,
+            5,
+            111,
+            137,
+            93,
+            227,
+            227,
+            35,
+            97,
+            184,
+            27,
+            55,
+            184,
+            200,
+            83,
+            179,
+            84,
+            84,
+            24,
+            11,
+            220,
+            121,
+            172,
+            88,
+            141,
+            69,
+            208,
+            1,
+            89,
+            252,
+            83,
+            0,
+            8,
           ],
           "type": "Buffer",
         }
