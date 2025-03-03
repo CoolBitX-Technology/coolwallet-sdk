9bc950665620400b7d5e1abe892db311cee737db
Hank Liu <hankliu@coolbitx.com>
2025-03-03 16:38:56 +0800

    update core dependency

--- packages/coin-cro/src/sign.ts
+++ packages/coin-cro/src/sign.ts
@@ -1,48 +1,41 @@
 import * as core from '@coolwallet/core';
-import * as types from './config/types'
-import * as txUtil from './utils/transactionUtils' 
+import * as types from './config/types';
+import * as txUtil from './utils/transactionUtils';
 
 export const signTransaction = async (
-    signData: types.SignDataType,
-    script: string,
-    argument: string
-  ): Promise<string> => {
-  
-    const { transport, appId, appPrivateKey, confirmCB, authorizedCB } = signData
-  
-    const preActions = [];
-    let action;
-    const sendScript = async () => {
-      await core.apdu.tx.sendScript(transport, script);
-    };
-    preActions.push(sendScript);
-  
-    action = async () => {
-      return core.apdu.tx.executeScript(
-        transport,
-        appId,
-        appPrivateKey,
-        argument
-      );
-    };
+  signData: types.SignDataType,
+  script: string,
+  argument: string
+): Promise<string> => {
+  const { transport, appId, appPrivateKey, confirmCB, authorizedCB } = signData;
 
-    const canonicalSignature = await core.tx.flow.getSingleSignatureFromCoolWallet(
-        transport,
-        preActions,
-        action,
-        false,
-        confirmCB,
-        authorizedCB,
-        true
-    );
+  const preActions = [];
+  const sendScript = async () => {
+    await core.apdu.tx.sendScript(transport, script);
+  };
+  preActions.push(sendScript);
 
-    const { signedTx } = await core.apdu.tx.getSignedHex(transport);
-    console.debug("signedTx: ", signedTx);
-  
-    if (!Buffer.isBuffer(canonicalSignature)) {
-        const croSignature = await txUtil.genCROSigFromSESig(canonicalSignature);
-        return croSignature;
-    } else {
-        throw new core.error.SDKError(signTransaction.name, 'canonicalSignature type error');
-    }
+  const action = async () => {
+    return core.apdu.tx.executeScript(transport, appId, appPrivateKey, argument);
+  };
+
+  const canonicalSignature = await core.tx.flow.getSingleSignatureFromCoolWallet(
+    transport,
+    preActions,
+    action,
+    false,
+    confirmCB,
+    authorizedCB,
+    true
+  );
+
+  const { signedTx } = await core.apdu.tx.getSignedHex(transport);
+  console.debug('signedTx: ', signedTx);
+
+  if (!Buffer.isBuffer(canonicalSignature)) {
+    const croSignature = await txUtil.genCROSigFromSESig(canonicalSignature);
+    return croSignature;
+  } else {
+    throw new core.error.SDKError(signTransaction.name, 'canonicalSignature type error');
+  }
 };
